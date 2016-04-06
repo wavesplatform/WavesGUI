d19574c230bf5951d2fb7cce542921082ece2c98
Tosch110 <tosch110@zedat.fu-berlin.de>
2016-04-07 01:38:41 +0200

    Waves 0.0.1
    First API Mockup

---
+++ js/functions.js
@@ -0,0 +1,380 @@
+"use strict";
+
+var server = 'http://23.94.190.226:9081';
+
+
+$(document).ready(function() {
+
+	initApp();
+	
+	$("#peerspage").on("click", function() {
+
+		loadPeers();
+
+	});
+
+	$("#blockchainpage").on("click", function() {
+		initApp();
+	});
+
+
+
+	$("#walletpage").on("click", function() {
+
+		loadWallet();
+		
+	});
+
+
+
+	$("#consensuspage").on("click", function() {
+
+		loadConsensus();
+		
+	});
+
+	$("#paymentpage").on("click", function() {
+
+		loadPayment();
+		
+	});
+
+	$("#debugpage").on("click", function() {
+
+		loadDebug();
+		
+	});
+
+	$(".wavesNavbar").on("click", function() {
+
+		$(".wavesNavbar").removeClass('active');
+
+		$(this).addClass('active');
+
+	});
+
+
+
+});
+
+function loadWallet() {
+
+	var appContainer;
+	var welcomeJumbo = '<div class="jumbotron"><h2>Your Waves Wallet</h2></div>';
+
+	appContainer = welcomeJumbo;
+
+	$.getJSON(server+'/addresses/', function(response) {
+
+		appContainer += '<div class="container"><h2>Your Wallet</h2>';
+
+		appContainer += '<table class="table table-striped">';
+		appContainer += '<thead><tr><th>Key</th><th>Value</th></tr></thead>';
+		appContainer += '<tbody>';
+
+		$.each(response, function(key, value) {
+
+			$.each(value, function(innerkey, innervalue) {
+
+				appContainer += '<tr><td>';
+				appContainer += innerkey;
+				appContainer += '</td><td>';
+				appContainer += innervalue;
+				appContainer += '</td></tr>';
+
+			});
+
+			
+
+		});
+
+		appContainer += '<tbody>';
+
+
+
+
+		appContainer += '</div>';
+
+		appContainer += '<button class="btn btn-primary" id="newAddress">New Address</button>';
+
+		
+		$("#app").html(appContainer);
+
+		$("#newAddress").on("click", function() {
+
+			$.post(server+'/addresses/', function(createAddress) {
+
+				loadWallet();
+
+			});
+
+		});
+
+
+	});
+
+}
+
+function loadPeers() {
+
+	var appContainer;
+	var welcomeJumbo = '<div class="jumbotron"><h2>All Waves Peers</h2></div>';
+
+	appContainer = welcomeJumbo;
+
+	$.getJSON(server+'/peers/all', function(response) {
+
+		appContainer += '<div class="container"><h2>Your peers</h2><button class="btn btn-primary" id="updatePeers">Update</button>';
+
+		appContainer += '<table class="table table-striped">';
+		appContainer += '<thead><tr><th>Key</th><th>Value</th></tr></thead>';
+		appContainer += '<tbody>';
+
+		$.each(response, function(key, value) {
+
+			$.each(value, function(innerkey, innervalue) {
+
+				appContainer += '<tr><td>';
+				appContainer += innerkey;
+				appContainer += '</td><td>';
+				appContainer += innervalue.address;
+				appContainer += '</td></tr>';
+
+			});
+
+			
+
+		});
+
+		appContainer += '<tbody>';
+
+		appContainer += '</div>';
+		
+		$("#app").html(appContainer);
+
+		$("#updatePeers").on("click", function() {
+			loadPeers();
+		});
+
+
+	});
+
+
+}
+
+function createTransactionTable (valueValue) {
+
+	var appContainer;
+
+	$.each(valueValue, function(transactionKey, transactionValue) {
+
+
+		appContainer += '<table class="table"></table>';
+
+		appContainer += '<thead><tr><th>Type</th><th>Fee</th><th>Timestamp</th><th>Amount</th><th>Sender</th><th>Recipient</th></tr></thead>';
+
+		appContainer += '<tbody><tr><td>'+transactionValue.type+'</td><td>'+transactionValue.fee+'</td><td>'+transactionValue.timestamp+'</td><td>'+transactionValue.amount+'</td><td>'+transactionValue.sender+'</td><td>'+transactionValue.recipient+'</td></tr></tbody>';
+
+		appContainer += '</table>';
+
+		return appContainer;
+
+
+
+	});
+
+}
+
+function initApp() {
+
+
+	var appContainer;
+	var welcomeJumbo = '<div class="jumbotron"><h2>Welcome to Waves Demo Platform</h2></div>';
+
+	appContainer = welcomeJumbo;
+
+	$.getJSON(server+'/blocks/last', function(response) {
+
+		appContainer += '<div class="container"><h2>Latest Block</h2><button class="btn btn-primary" id="updateBlock">Update</button>';
+
+		appContainer += '<table class="table table-striped">';
+		appContainer += '<thead><tr><th>Key</th><th>Value</th></tr></thead>';
+		appContainer += '<tbody>';
+
+		$.each(response, function(key, value) {
+
+		
+				appContainer += '<tr><td>';
+				appContainer += key;
+				appContainer += '</td><td>';
+				appContainer += value;
+				appContainer += '</td></tr>';
+
+
+		});
+
+		appContainer += '</tbody>';
+
+		appContainer += '</div>';
+		
+		$("#app").html(appContainer);
+		
+		$("#updateBlock").on("click", function() {
+			initApp();
+		});
+
+
+	});
+
+
+}
+
+
+function loadConsensus() {
+
+
+	var appContainer;
+	var welcomeJumbo = '<div class="jumbotron"><h2>Waves Consensus</h2></div>';
+
+	appContainer = welcomeJumbo;
+
+	$.getJSON(server+'/consensus/algo', function(response) {
+
+		appContainer += '<div class="container"><h2>Consensus Type</h2>';
+
+		appContainer += '<table class="table table-striped">';
+		appContainer += '<thead><tr><th>Key</th><th>Value</th></tr></thead>';
+		appContainer += '<tbody>';
+
+		$.each(response, function(key, value) {
+
+			appContainer += '<tr><td>';
+			appContainer += key;
+			appContainer += '</td><td>';
+			appContainer += value;
+			appContainer += '</td></tr>';
+
+		});
+
+		appContainer += '</tbody>';
+		appContainer += '</table>';
+
+		
+		appContainer += '<h2>Current Target</h2><button class="btn btn-primary" id="updateTarget">Update</button>';
+
+
+		appContainer += '<table class="table table-striped">';
+		appContainer += '<thead><tr><th>Key</th><th>Value</th></tr></thead>';
+		appContainer += '<tbody>';
+
+		$.getJSON('http://23.94.190.226:9081/consensus/target', function (response_target) {
+
+
+			$.each(response_target, function(key, value) {
+
+				appContainer += '<tr><td>';
+				appContainer += key;
+				appContainer += '</td><td>';
+				appContainer += value;
+				appContainer += '</td></tr>';
+
+			});
+
+			appContainer += '</tbody>';
+			appContainer += '</table>';
+
+			appContainer += '</div>';
+
+			$("#app").html(appContainer);
+
+			$("#updateTarget").on("click", function() {
+				loadConsensus();
+			});
+
+
+
+		});
+		
+
+
+	});
+
+}
+
+function loadPayment () {
+
+	var paymentForm  = '<form class="col-lg-3 col-md-3">'+
+						 '<div class="form-group">'+
+						  '  <label for="exampleInputEmail1">Recipient</label>'+
+						  '  <input type="email" class="form-control" id="recipient" placeholder="Recipient">'+
+						  '</div>'+
+						  '<div class="form-group">'+
+						  ' <label for="exampleInputPassword1">Amount</label>'+
+						  '  <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Amount" min="0">'+
+						  '</div>'+
+						  '<hr>'+
+						  '<p>+1 Fee</p>'+
+						  '<button type="submit" class="btn btn-default">Submit</button>'+
+						'</form>';
+
+
+
+
+	$("#app").html(paymentForm);
+
+
+
+
+}
+
+function loadDebug () {
+
+	var debugPage = '<div class="container">'+
+	'<div class="row"><h2>Settings File</h2>';
+
+	debugPage += '<table class="table table-striped">';
+	debugPage += '<thead><tr><th>Key</th><th>Value</th></tr></thead>';
+	debugPage += '<tbody>';
+
+	$.getJSON('http://23.94.190.226:9081/debug/settings', function (response) {
+
+		$.each(response, function(key, value) {
+
+			debugPage += '<tr><td>';
+			debugPage += key;
+			debugPage += '</td><td>';
+			debugPage += value;
+			debugPage += '</td></tr>';
+
+		});
+
+		debugPage += '</tbody>';
+		debugPage += '</table>';
+
+		debugPage += '<h2>Info</h2>';
+
+		$.getJSON('http://23.94.190.226:9081/debug/info', function (response_info) {
+
+			$.each(response_info, function(key, value) {
+
+				debugPage += '<tr><td>';
+				debugPage += key;
+				debugPage += '</td><td>';
+				debugPage += value;
+				debugPage += '</td></tr>';
+
+			});
+
+
+			debugPage += '</tbody>';
+			debugPage += '</table>';
+
+
+			$("#app").html(debugPage);
+
+		});
+
+	});
+
+
+}
