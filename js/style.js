806f99cdf2845076d325ca9ae5463d75fa21d586
Tosch110 <tosch110@zedat.fu-berlin.de>
2016-05-16 21:52:31 +0200

    notices
    copyright notice edit

--- js/style.js
+++ js/style.js
@@ -1,12 +1,12 @@
 /******************************************************************************
- * Copyright © 2016 The Nxt Core Developers.                             *
+ * Copyright © 2016 The Waves Core Developers.                                *
  *                                                                            *
- * See the AUTHORS.txt, DEVELOPER-AGREEMENT.txt and LICENSE.txt files at      *
+ * See the LICENSE.txt files at                                               *
  * the top-level directory of this distribution for the individual copyright  *
  * holder information and the developer policies on copyright and licensing.  *
  *                                                                            *
  * Unless otherwise agreed in a custom licensing agreement, no part of the    *
- * Nxt software, including this file, may be copied, modified, propagated,    *
+ * Waves software, including this file, may be copied, modified, propagated,  *
  * or distributed except according to the terms contained in the LICENSE.txt  *
  * file.                                                                      *
  *                                                                            *
