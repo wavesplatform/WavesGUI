a59f020843cf81a448f4f73dc6b9b68a33547888
Tosch110 <tosch110@zedat.fu-berlin.de>
2016-05-25 19:11:58 +0200

    title in address

--- js/waves.ui.functions.js
+++ js/waves.ui.functions.js
@@ -263,7 +263,7 @@ var Waves = (function(Waves, $, undefined) {
         Waves.password = accountDetails.password;
         Waves.checksum = accountDetails.checksum;
 
-        $("#wavesAccountAddress").html('<span class="clipSpan tooltip-1" title="Copy this address to the clipboard." data-clipboard-text="'+Waves.address+'" style="cursor: pointer; cursor: hand;">'+Waves.address+'</span>')
+        $("#wavesAccountAddress").html('<span class="clipSpan tooltip-1" data-clipboard-text="'+Waves.address+'" style="cursor: pointer; cursor: hand;">'+Waves.address+'</span>')
 
         Waves.loadAddressBalance(Waves.address, function (balance) {
 
