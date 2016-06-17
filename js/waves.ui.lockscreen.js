74b742289f48cabe0bc51c75b475bd70272c3be5
Tosch110 <tosch110@zedat.fu-berlin.de>
2016-06-17 17:11:10 +0200

    disabled textarea

--- js/waves.ui.lockscreen.js
+++ js/waves.ui.lockscreen.js
@@ -115,7 +115,6 @@ var Waves = (function(Waves, $, undefined) {
 
         $("#step2_reg").show();
         $("#login-wPop-new").modal("show");
-        $("#walletSeed").prop('disabled', true);
         NProgress.start();
        
     });
