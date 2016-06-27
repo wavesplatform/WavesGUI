bbc9805bf3831be1e272e0d351d72f55a749020a
beregovoy68 <s.a.ishchenko@gmail.com>
2016-06-27 18:07:48 +0300

    Fixed commission is now equal to 0.001 waves.` Added methods to convert from waves to wavelets and back. Definitely need a money class for waves. Added some tests.

--- js/waves.constants.js
+++ js/waves.constants.js
@@ -44,6 +44,7 @@ var Waves = (function (Waves, $) {
         "MAINNET_ADDRESS_REGEXP": /^[a-zA-Z0-9]{35}$/,
 
         "INITIAL_NONCE": 0,
+        "WAVELETS_IN_WAVE": 1e8,
     };
 
     if (Waves.UI === undefined)
@@ -51,6 +52,7 @@ var Waves = (function (Waves, $) {
 
     Waves.UI.constants = {
         'MINIMUM_PAYMENT_AMOUNT' : 1e-8,
+        'MINIMUM_TRANSACTION_FEE' : 0.001,
         'AMOUNT_DECIMAL_PLACES' : 8
     }
     
