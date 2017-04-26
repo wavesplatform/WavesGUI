ceaac7cf06fb01049e21b664bf26caa0331087f9
beregovoy68 <s.a.ishchenko@gmail.com>
2017-04-26 17:42:30 +0300

    Leasing of Waves is back

--- src/js/wallet/list.controller.js
+++ src/js/wallet/list.controller.js
@@ -40,8 +40,8 @@
             },
             {
                 balance: new Money(0, Currency.WAV),
-                depositWith: Currency.BTC/*, TODO: uncomment when released
-                leasingAvailable: true*/
+                depositWith: Currency.BTC,
+                leasingAvailable: true
             },
             {
                 balance: new Money(0, Currency.CNY),
