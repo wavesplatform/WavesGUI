7f7569bc2eb5a81e4edc6c571cbd96027c2e461c
beregovoy68 <s.a.ishchenko@gmail.com>
2017-05-21 22:19:58 +1000

    First desktop application version that runs

--- src/desktop/main.js
+++ src/desktop/main.js
@@ -13,7 +13,9 @@ let mainWindow;
 
 function createWindow () {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
+        nodeIntegration: false
+    }});
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
