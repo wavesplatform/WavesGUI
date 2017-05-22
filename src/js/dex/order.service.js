28815cc65a438f1741c3c7597a01e5a5adde0a6a
xenohunter <xenohunter@yandex.ru>
2017-05-22 15:42:40 +0300

    Added order data checks

--- src/js/dex/order.service.js
+++ src/js/dex/order.service.js
@@ -22,7 +22,7 @@
                     var signedRequest = matcherRequestService.buildCreateOrderRequest(order, sender);
                     return matcherApiService.createOrder(signedRequest);
                 }).catch(function (e) {
-                    throw new Error(); // TODO
+                    throw new Error(e); // TODO
                 });
         };
 
@@ -46,7 +46,7 @@
                             throw new Error();
                         }
                     }).catch(function (e) {
-                        throw new Error(); // TODO
+                        throw new Error(e); // TODO
                     });
             }
         };
@@ -58,22 +58,30 @@
                     privateKey: applicationContext.account.keyPair.private
                 })
                 .then(function (response) {
-                    return response.map(function (order) {
-                        var orderPrice = OrderPrice.fromBackendPrice(order.price, pair).toTokens();
+                    return response.map(function (o) {
+                        if (o.amount === null || o.price === null || o.filled === null || o.timestamp === null) {
+                            console.error('Bad order!', o);
+                            o.amount = o.amount || 0;
+                            o.price = o.price || 0;
+                            o.filled = o.filled || 0;
+                            o.timestamp = o.timestamp || 0;
+                        }
+
+                        var orderPrice = OrderPrice.fromBackendPrice(o.price, pair).toTokens();
 
                         return {
-                            id: order.id,
-                            type: order.type,
+                            id: o.id,
+                            type: o.type,
                             price: Money.fromTokens(orderPrice, pair.priceAsset),
-                            amount: Money.fromCoins(order.amount, pair.amountAsset),
-                            filled: Money.fromCoins(order.filled, pair.amountAsset),
-                            status: order.status || NOT_FOUND,
-                            timestamp: order.timestamp
+                            amount: Money.fromCoins(o.amount, pair.amountAsset),
+                            filled: Money.fromCoins(o.filled, pair.amountAsset),
+                            status: o.status || NOT_FOUND,
+                            timestamp: o.timestamp
                         };
                     });
                 })
                 .catch(function (e) {
-                    throw new Error(); // TODO
+                    throw new Error(e); // TODO
                 });
         };
     }
