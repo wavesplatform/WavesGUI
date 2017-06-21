e8ead7d935769ab4dccee41f5e184228fdf1f459
xenohunter <xenohunter@yandex.ru>
2017-06-21 18:18:08 +0300

    Fixed code style

--- src/js/navigation/main.menu.controller.js
+++ src/js/navigation/main.menu.controller.js
@@ -3,26 +3,26 @@
 
     function MainMenuController($scope, $interval, events, applicationContext,
                                 cryptoService, dialogService, notificationService, apiService) {
-        var refreshPromise, delayRefresh = 10 * 1000;
-        var menu = this;
+        var ctrl = this,
+            refreshPromise,
+            delayRefresh = 10 * 1000;
 
-        menu.blockHeight = 0;
-        menu.address = applicationContext.account.address;
+        ctrl.blockHeight = 0;
+        ctrl.address = applicationContext.account.address;
 
         function initializeBackupFields() {
-            menu.seed = applicationContext.account.seed;
-            menu.encodedSeed = cryptoService.base58.encode(converters.stringToByteArray(menu.seed));
-            menu.publicKey = applicationContext.account.keyPair.public;
-            menu.privateKey = applicationContext.account.keyPair.private;
+            ctrl.seed = applicationContext.account.seed;
+            ctrl.encodedSeed = cryptoService.base58.encode(converters.stringToByteArray(ctrl.seed));
+            ctrl.publicKey = applicationContext.account.keyPair.public;
+            ctrl.privateKey = applicationContext.account.keyPair.private;
         }
 
         function buildBackupClipboardText() {
-            var text = 'Seed: ' + menu.seed + '\n';
-            text += 'Encoded seed: ' + menu.encodedSeed + '\n';
-            text += 'Private key: ' + menu.privateKey + '\n';
-            text += 'Public key: ' + menu.publicKey + '\n';
-            text += 'Address: ' + menu.address;
-
+            var text = 'Seed: ' + ctrl.seed + '\n';
+            text += 'Encoded seed: ' + ctrl.encodedSeed + '\n';
+            text += 'Private key: ' + ctrl.privateKey + '\n';
+            text += 'Public key: ' + ctrl.publicKey + '\n';
+            text += 'Address: ' + ctrl.address;
             return text;
         }
 
@@ -36,9 +36,9 @@
             }
         });
 
-        menu.showBackupDialog = showBackupDialog;
-        menu.showProfileDialog = showProfileDialog;
-        menu.backup = backup;
+        ctrl.showBackupDialog = showBackupDialog;
+        ctrl.showProfileDialog = showProfileDialog;
+        ctrl.backup = backup;
 
         function showProfileDialog () {
             $scope.$broadcast(events.NAVIGATION_CREATE_ALIAS, {});
@@ -51,28 +51,30 @@
 
         function backup() {
             var clipboard = new Clipboard('#backupForm', {
-                text: function (trigger) {
+                text: function () {
                     return buildBackupClipboardText();
                 }
             });
+
             clipboard.on('success', function(e) {
                 notificationService.notice('Account backup has been copied to clipboard');
                 e.clearSelection();
             });
+
             angular.element('#backupForm').click();
             clipboard.destroy();
         }
 
         function refreshBlockHeight() {
             apiService.blocks.height().then(function (response) {
-                menu.blockHeight = response.height;
+                ctrl.blockHeight = response.height;
                 applicationContext.blockHeight = response.height;
             });
         }
     }
 
     MainMenuController.$inject = ['$scope', '$interval', 'navigation.events', 'applicationContext',
-        'cryptoService', 'dialogService', 'notificationService', 'apiService'];
+                                  'cryptoService', 'dialogService', 'notificationService', 'apiService'];
 
     angular
         .module('app.navigation')
