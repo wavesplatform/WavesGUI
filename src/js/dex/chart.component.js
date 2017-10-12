efbd5cb2c4870ccd4a389c249848f5e8a4f339e8
xenohunter <xenohunter@yandex.ru>
2017-10-12 15:46:15 +0300

    CLIENT-32: fixed charts fake low price issue

--- src/js/dex/chart.component.js
+++ src/js/dex/chart.component.js
@@ -5,6 +5,24 @@
         CANDLE_FRAME = 30,
         POLLING_DELAY = 5000;
 
+    function isCandleEmpty(c) {
+        return +c.open === 0 && +c.high === 0 && +c.low === 0 && +c.close === 0 && +c.vwap === 0;
+    }
+
+    function adjustCandles(candles) {
+
+        var i = candles.length;
+        while (isCandleEmpty(candles[--i])) {}
+
+        var fix = candles[i].open;
+        while (++i < candles.length) {
+            candles[i].open = candles[i].high = candles[i].low = candles[i].close = candles[i].vwap = fix;
+        }
+
+        return candles;
+
+    }
+
     function ChartController($element, $interval, datafeedApiService, utilsService, chartsFactory) {
         var ctrl = this,
             intervalPromise;
@@ -38,6 +56,7 @@
                 datafeedApiService
                     .getLastCandles(pair, CANDLE_NUMBER, CANDLE_FRAME)
                     .then(function (response) {
+                        response = adjustCandles(response);
                         ctrl.chart.draw(response);
                     });
             }
