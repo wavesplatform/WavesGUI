928f07ecce4ca6c0a25f22c7f8b4f77ba4aa22ae
Tsigel <tsddaniil@gmail.com>
2017-11-01 18:14:48 +0300

    CLIENT-76: add terms-accept modal

--- src/modules/ui/directives/checkbox/Checkbox.js
+++ src/modules/ui/directives/checkbox/Checkbox.js
@@ -1,7 +1,12 @@
 (function () {
     'use strict';
 
-    const controller = function (Base) {
+    /**
+     * @param Base
+     * @param {JQuery} $element
+     * @return {Checkbox}
+     */
+    const controller = function (Base, $element) {
 
         class Checkbox extends Base {
 
@@ -15,6 +20,10 @@
                  * @type {boolean}
                  */
                 this.disabled = false;
+            }
+
+            $postLink() {
+                $element.removeAttr('id');
             }
 
         }
