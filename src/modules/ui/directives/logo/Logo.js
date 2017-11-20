591dccf451a1b219f923e443e696b2bced8675c5
xenohunter <xenohunter@yandex.ru>
2017-11-20 20:21:32 +0300

    Added new loader

--- src/modules/ui/directives/logo/Logo.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app.ui').component('wLogo', {
-        templateUrl: 'modules/ui/directives/logo/logo.html'
-    });
-})();
--- src/modules/ui/directives/logo/logo.html
+++
@@ -1,1 +0,0 @@
-<div class="logo"></div>
