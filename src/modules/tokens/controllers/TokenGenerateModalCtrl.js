7da326fb6740fffca34725269d8e45b997a61d6d
tsigel <tsddaniil@gmail.com>
2017-11-27 20:14:58 +0300

    CLIENT-144: create token page

--- src/modules/tokens/controllers/TokenGenerateModalCtrl.js
+++ src/modules/tokens/controllers/TokenGenerateModalCtrl.js
@@ -1,23 +1,87 @@
 (function () {
     'use strict';
 
-    const controller = function (Base, $scope) {
+    /**
+     * @param Base
+     * @param $scope
+     * @param {app.user} user
+     * @param {app.i18n} i18n
+     * @return {TokenGenerateModalCtrl}
+     */
+    const controller = function (Base, $scope, user, i18n) {
 
         class TokenGenerateModalCtrl extends Base {
 
-            constructor({ amount, name, precision }) {
+            /**
+             * @param {string} amount
+             * @param {string} name
+             * @param {string} precision
+             * @param {boolean} issue
+             * @param {string} shownAmount
+             */
+            constructor({ amount, name, precision, issue, shownAmount }) {
                 super($scope);
+                /**
+                 * Modal step (confirm/success)
+                 * @type {number}
+                 */
+                this.step = 0;
+                /**
+                 * Tokens count (string from BigNumber)
+                 * @type {string}
+                 */
                 this.amount = amount;
+                /**
+                 * Formatted and localized tokens count
+                 * @type {string}
+                 */
+                this.shownAmount = shownAmount;
+                /**
+                 * Token name
+                 * @type {string}
+                 */
                 this.name = name;
+                /**
+                 * Token precision
+                 * @type {number}
+                 */
                 this.precision = precision;
+                /**
+                 * Modal title text
+                 * @type {null}
+                 */
                 this.title = null;
+                /**
+                 * Can token reissue
+                 * @type {boolean}
+                 */
+                this.issue = issue;
+
+                this.observe('step', this._onChangeStep);
+                this._onChangeStep();
+            }
+
+            _onChangeStep() {
+                const step = this.step;
+
+                switch (step) {
+                    case 0:
+                        this.title = i18n.translate('modal.title', 'app.tokens', { name: this.name });
+                        break;
+                    case 1:
+                        this.title = i18n.translate('modal.titleSuccess', 'app.tokens', { name: this.name });
+                        break;
+                    default:
+                        throw new Error('Wrong step index!');
+                }
             }
         }
 
         return new TokenGenerateModalCtrl(this.locals);
     };
 
-    controller.$inject = ['Base', '$scope'];
+    controller.$inject = ['Base', '$scope', 'user', 'i18n'];
 
-    angular.module('app.tokens').controller('TokenGenerateModalCtrl', controller);
+    angular.module('app.tokens')
+        .controller('TokenGenerateModalCtrl', controller);
 })();
--- src/modules/tokens/controllers/TokensCtrl.js
+++ src/modules/tokens/controllers/TokensCtrl.js
@@ -13,20 +13,49 @@
 
             constructor() {
                 super($scope);
+                /**
+                 * Token name
+                 * @type {string}
+                 */
                 this.name = '';
+                /**
+                 * Token description
+                 * @type {string}
+                 */
                 this.description = '';
+                /**
+                 * Can reissue this token
+                 * @type {boolean}
+                 */
                 this.issue = true;
+                /**
+                 * Count of generated tokens
+                 * @type {BigNumber}
+                 */
                 this.count = null;
+                /**
+                 * Precision of token
+                 * @type {BigNumber}
+                 */
                 this.precision = null;
             }
 
             generate() {
+                const precision = Number(this.precision.toString());
+
                 modalManager.showCustomModal({
                     ns: 'app.tokens',
                     controller: 'TokenGenerateModalCtrl',
                     titleContent: '{{$ctrl.title}}',
                     mod: 'tokens-generate-modal',
-                    contentUrl: '/modules/tokens/templates/generate.modal.htm.html'
+                    contentUrl: 'modules/tokens/templates/generate.modal.html',
+                    locals: {
+                        shownAmount: this.count.toFormat(precision),
+                        amount: this.count.toFixed(precision),
+                        name: this.name,
+                        issue: this.issue,
+                        precision
+                    }
                 });
             }
 
@@ -37,6 +66,7 @@
 
     controller.$inject = ['Base', '$scope', 'modalManager'];
 
-    angular.module('app.tokens').controller('TokensCtrl', controller);
+    angular.module('app.tokens')
+        .controller('TokensCtrl', controller);
 })();
 
