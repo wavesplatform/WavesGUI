d048eca75c191e871869c03c21b399f6454f2c58
Tsigel <tsddaniil@gmail.com>
2017-11-28 13:13:01 +0300

    CLIENT-144: fix balance for leasing

--- src/modules/app/services/waves/node/Node.js
+++ src/modules/app/services/waves/node/Node.js
@@ -37,11 +37,24 @@
             get() {
                 return Waves.API.Node.v2.addresses.get(user.address)
                     .then(({ wavesBalance }) => {
-                        return {
-                            leasedOut: wavesBalance.leasedOut,
-                            leasedIn: wavesBalance.leasedIn,
-                            available: eventManager.updateBalance(wavesBalance.available)
-                        };
+
+                        const available = eventManager.updateBalance(wavesBalance.available);
+
+                        if (available.getTokens().lt(0)) {
+                            return Waves.Money.fromTokens('0', available.asset.id).then((available) => {
+                                return {
+                                    leasedOut: wavesBalance.leasedOut,
+                                    leasedIn: wavesBalance.leasedIn,
+                                    available
+                                };
+                            });
+                        } else {
+                            return {
+                                leasedOut: wavesBalance.leasedOut,
+                                leasedIn: wavesBalance.leasedIn,
+                                available: eventManager.updateBalance(wavesBalance.available)
+                            };
+                        }
                     });
             }
 
