28e854df0421fc434605a7521c39f69dde721edd
xenohunter <xenohunter@yandex.ru>
2017-12-08 13:21:40 +0300

    Added service to work with Coinomat

---
+++ src/modules/utils/services/CoinomatService.js
@@ -0,0 +1,81 @@
+(function () {
+    'use strict';
+
+    const PATH = `${WavesApp.network.coinomat}/api/v1`;
+    const LANGUAGE = 'ru_RU';
+
+    const CURRENCIES = {
+        // TODO : move this list to a server-size DB
+        [WavesApp.defaultAssets.BTC]: { waves: 'WBTC', coinomat: 'BTC' },
+        [WavesApp.defaultAssets.ETH]: { waves: 'WETH', coinomat: 'ETH' },
+        [WavesApp.defaultAssets.LTC]: { waves: 'WLTC', coinomat: 'LTC' },
+        [WavesApp.defaultAssets.ZEC]: { waves: 'WZEC', coinomat: 'ZEC' }
+    };
+
+    /**
+     * @return {CoinomatService}
+     */
+    const factory = function () {
+
+        class CoinomatService {
+
+            // From Coinomat to Waves
+            getDepositDetails(assetId, wavesAddress) {
+                CoinomatService._isSupportedAsset(assetId);
+                return this._loadPaymentDetails(CURRENCIES[assetId].coinomat, CURRENCIES[assetId].waves, wavesAddress);
+            }
+
+            // From Waves to Coinomat
+            getWithdrawDetails(assetId, wavesAddress) {
+                CoinomatService._isSupportedAsset(assetId);
+                return this._loadPaymentDetails(CURRENCIES[assetId].waves, CURRENCIES[assetId].coinomat, wavesAddress);
+            }
+
+            getSupportedAssetIdList() {
+                return Object.keys(CURRENCIES);
+            }
+
+            _loadPaymentDetails(from, to, recipientAddress) {
+                return $.get(`${PATH}/create_tunnel.php`, {
+                    currency_from: from,
+                    currency_to: to,
+                    wallet_to: recipientAddress
+                }).then((res) => {
+                    CoinomatService._isEligibleResponse(res, 'ok');
+                    return $.get(`${PATH}/get_tunnel.php`, {
+                        xt_id: res.tunnel_id,
+                        k1: res.k1,
+                        k2: res.k2,
+                        history: 0,
+                        lang: LANGUAGE
+                    });
+                }).then((res) => {
+                    CoinomatService._isEligibleResponse(res, 'tunnel');
+                    return {
+                        address: res.tunnel.wallet_from,
+                        attachment: res.tunnel.attachment
+                    };
+                });
+            }
+
+            static _isEligibleResponse(response, fieldName) {
+                if (!response[fieldName]) {
+                    throw new Error(response.error);
+                } else {
+                    return response;
+                }
+            }
+
+            static _isSupportedAsset(assetId) {
+                if (!CURRENCIES[assetId]) {
+                    throw new Error('Asset is not supported by Coinomat');
+                }
+            }
+
+        }
+
+        return new CoinomatService();
+    };
+
+    angular.module('app.utils').factory('coinomatService', factory);
+})();
