c319c1afb97a5257aba061d95b17a94fa2dd5173
xenohunter <xenohunter@yandex.ru>
2017-12-21 17:40:59 +0300

    CLIENT-292: added WNET and EFYT icons and bumped version

--- src/modules/ui/directives/assetLogo/AssetLogo.js
+++ src/modules/ui/directives/assetLogo/AssetLogo.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
+    // TODO @xenohunter : remove that when icons are in @dvshur's service
     const ASSET_IMAGES_MAP = {
         [WavesApp.defaultAssets.WAVES]: '/img/assets/waves.svg',
         [WavesApp.defaultAssets.BTC]: '/img/assets/bitcoin.svg',
@@ -8,7 +9,9 @@
         [WavesApp.defaultAssets.LTC]: '/img/assets/ltc.svg',
         [WavesApp.defaultAssets.ZEC]: '/img/assets/zec.svg',
         [WavesApp.defaultAssets.EUR]: '/img/assets/euro.svg',
-        [WavesApp.defaultAssets.USD]: '/img/assets/usd.svg'
+        [WavesApp.defaultAssets.USD]: '/img/assets/usd.svg',
+        [WavesApp.otherAssetsWithIcons.EFYT]: '/img/assets/efyt.svg',
+        [WavesApp.otherAssetsWithIcons.WNET]: '/img/assets/wnet.svg'
     };
 
     const COLORS_MAP = {
