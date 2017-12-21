4403d61e2629fe6f4d44b840be588bad54e94666
xenohunter <xenohunter@yandex.ru>
2017-12-21 16:13:49 +0300

    CLIENT-292: added icons for LTC and ZEC

--- src/modules/ui/directives/assetLogo/AssetLogo.js
+++ src/modules/ui/directives/assetLogo/AssetLogo.js
@@ -5,6 +5,8 @@
         [WavesApp.defaultAssets.WAVES]: '/img/assets/waves.svg',
         [WavesApp.defaultAssets.BTC]: '/img/assets/bitcoin.svg',
         [WavesApp.defaultAssets.ETH]: '/img/assets/ethereum.svg',
+        [WavesApp.defaultAssets.LTC]: '/img/assets/ltc.svg',
+        [WavesApp.defaultAssets.ZEC]: '/img/assets/zec.svg',
         [WavesApp.defaultAssets.EUR]: '/img/assets/euro.svg',
         [WavesApp.defaultAssets.USD]: '/img/assets/usd.svg'
     };
