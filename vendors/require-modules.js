767bcafeb26d555b31e6a5606febb8df2b08fa02
tsigel <tsddaniil@gmail.com>
2017-12-22 17:52:00 +0300

    CLIENT-334: add electron deps

--- vendors/require-modules.js
+++ vendors/require-modules.js
@@ -7,7 +7,30 @@
         'ts-api-validator': 'tsApiValidator'
     };
 
-    window.require = function (name) {
+    if (window.require) {
+        Object.keys(MODULES_MAP).forEach((nodeName) => {
+            const winName = MODULES_MAP[nodeName];
+            window[winName] = require(nodeName);
+        });
+        const origin = require;
+        window.require = function (name) {
+            if (name in MODULES_MAP) {
+                return window[MODULES_MAP[name]];
+            } else {
+                return origin(name);
+            }
+        };
+    } else {
+        window.require = function (name) {
+            if (name in MODULES_MAP) {
+                return window[MODULES_MAP[name]];
+            } else {
+                throw new Error(`Not loaded module with name "${name}"`);
+            }
+        };
+    }
+
+    window.require = window.require || function (name) {
         if (name in MODULES_MAP) {
             return window[MODULES_MAP[name]];
         } else {
