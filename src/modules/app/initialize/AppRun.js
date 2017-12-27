b3667fb71bcf9b7a43eab194521bec468a9ac432
xenohunter <xenohunter@yandex.ru>
2017-12-27 17:46:47 +0300

    CLIENT-334: recommitted Electron merge commit

--- src/modules/app/initialize/AppRun.js
+++ src/modules/app/initialize/AppRun.js
@@ -235,8 +235,7 @@
              * @private
              */
             _getImagesReadyPromise() {
-                return fetch(`/img/images-list.json?v=${WavesApp.version}`)
-                    .then((r) => r.json())
+                return $.ajax({ url: `/img/images-list.json?v=${WavesApp.version}`, dataType: 'json' })
                     .then((list) => {
                         return Promise.all(list.map(AppRun.getLoadImagePromise(list.length)));
                     });
