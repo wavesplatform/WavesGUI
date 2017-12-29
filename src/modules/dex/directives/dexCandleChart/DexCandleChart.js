941d441fae09173bf29585b0cca3fee7180d88f5
xenohunter <xenohunter@yandex.ru>
2017-12-29 17:35:06 +0300

    Changed TradingVoew path once again

--- src/modules/dex/directives/dexCandleChart/DexCandleChart.js
+++ src/modules/dex/directives/dexCandleChart/DexCandleChart.js
@@ -65,7 +65,7 @@
                         interval: WavesApp.dex.defaultResolution,
                         container_id: this.elementId,
                         datafeed: candlesService,
-                        library_path: 'https://jslib.wavesnodes.com/',
+                        library_path: 'trading-view/',
                         autosize: true,
                         disabled_features: DISABLED_FEATURES
                     });
