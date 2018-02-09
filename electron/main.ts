a81fb2aa0eb83f438ade240dc356e556e6d87528
tsigel <tsddaniil@gmail.com>
2018-02-09 09:28:07 +0300

    CLIENT-443: add electron menu

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,11 @@
-import { app, BrowserWindow, screen, protocol } from 'electron';
+import { app, BrowserWindow, screen, protocol, Menu } from 'electron';
 import { ISize, IMetaJSON } from './package';
 import { format } from 'url';
 import { readFile, stat, writeFile } from 'fs';
 import { join } from 'path';
 
 import BrowserWindowConstructorOptions = Electron.BrowserWindowConstructorOptions;
+import MenuItemConstructorOptions = Electron.MenuItemConstructorOptions;
 
 const CONFIG = {
     META_PATH: join(app.getPath('userData'), 'meta.json'),
@@ -25,6 +26,28 @@ const CONFIG = {
     }
 };
 
+const MENU_LIST: MenuItemConstructorOptions[] = [
+    {
+        label: 'Application',
+        submenu: [
+            { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+            { type: 'separator' },
+            { label: 'Quit', accelerator: 'Command+Q', click: () => app.quit() }
+        ]
+    } as MenuItemConstructorOptions, {
+        label: 'Edit',
+        submenu: [
+            { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+            { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+            { type: 'separator' },
+            { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+            { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+            { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+            { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+        ]
+    } as MenuItemConstructorOptions
+];
+
 
 class Main {
 
@@ -34,6 +57,8 @@ class Main {
         this.mainWindow = null;
 
         this.setHandlers();
+
+        Menu.setApplicationMenu(Menu.buildFromTemplate(MENU_LIST));
     }
 
     private createWindow() {
