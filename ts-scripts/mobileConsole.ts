f40afcdd50ce97faa0576b4579741d3f6325ff1e
tsigel <tsddaniil@gmail.com>
2018-02-09 14:55:48 +0300

    CLIENT-479: fix console for mobile

--- ts-scripts/mobileConsole.ts
+++ ts-scripts/mobileConsole.ts
@@ -1,24 +1,62 @@
-window.addEventListener('load', function () {
+(function () {
 
     if (location.href.indexOf('console=false') !== -1) {
         return null;
     }
 
-    const out = document.createElement('DIV');
-    $(out).css({
+    var css = function (element: HTMLElement, styles) {
+        Object.keys(styles).forEach((styleName) => {
+            var value = styles[styleName];
+            element.style[styleName] = value;
+        });
+    };
+
+    var out = document.createElement('DIV');
+    css(out, {
         position: 'absolute',
-        top: '10px',
-        lef: '10px',
+        top: '10%',
+        left: '10%',
         fontSize: '25px',
         padding: '20px',
-        zIndex: 100000
+        lineHeight: '30px',
+        width: '80%',
+        zIndex: 100000,
+        borderRadius: '10px',
+        border: '1px solid #000',
+        backgroundColor: '#eee'
     });
     document.body.appendChild(out);
 
     window.onerror = function (message: string, filename?: string, lineno?: number, colno?: number, error?: Error) {
-        const line = document.createElement('div');
+        var line = document.createElement('div');
         line.innerText = `Message: ${message}\nFile name: ${filename}\nLine: ${lineno}\nStack: ${error.stack}`;
         out.appendChild(line);
     };
 
-});
+    ['log', 'warn', 'error'].forEach((methodName) => {
+        var method = console[methodName];
+        console[methodName] = function () {
+            method.apply(console, arguments);
+            var line = document.createElement('DIV');
+            line.innerText = `Type: ${methodName}\nMessage: ${Array.prototype.join.call(arguments, ', ')}`;
+            out.appendChild(line);
+        };
+    });
+
+    if (!Object.values) {
+        Object.values = function (target) {
+            return Object.keys(target).map(function (name) {
+                return target[name];
+            });
+        };
+    }
+
+    if (typeof Promise === 'undefined') {
+        console.error('No Promise!');
+    }
+
+    if (typeof crypto === 'undefined') {
+        console.error('No crypto!');
+    }
+
+})();
