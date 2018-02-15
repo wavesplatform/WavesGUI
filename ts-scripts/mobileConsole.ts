d3e665cc8ea5ff0e26648b3a73ca3287e0e3a3dc
tsigel <tsddaniil@gmail.com>
2018-02-15 16:21:49 +0300

    CLIENT-406: fix console

--- ts-scripts/mobileConsole.ts
+++ ts-scripts/mobileConsole.ts
@@ -1,24 +1,25 @@
 window.addEventListener('load', function () {
 
-    if (location.href.indexOf('console=false') !== -1) {
-        return null;
+    if (location.href.indexOf('console=true') !== -1) {
+
+        const out = document.createElement('DIV');
+        $(out).css({
+            position: 'absolute',
+            top: '10px',
+            lef: '10px',
+            fontSize: '25px',
+            padding: '20px',
+            zIndex: 100000
+        });
+        document.body.appendChild(out);
+
+        const origin = window.onerror || (() => null);
+        window.onerror = function (message: string, filename?: string, lineno?: number, colno?: number, error?: Error) {
+            origin(message, filename, lineno, colno, error);
+            const line = document.createElement('div');
+            line.innerText = `Message: ${message}\nFile name: ${filename}\nLine: ${lineno}\nStack: ${error.stack}`;
+            out.appendChild(line);
+        };
+
     }
-
-    const out = document.createElement('DIV');
-    $(out).css({
-        position: 'absolute',
-        top: '10px',
-        lef: '10px',
-        fontSize: '25px',
-        padding: '20px',
-        zIndex: 100000
-    });
-    document.body.appendChild(out);
-
-    window.onerror = function (message: string, filename?: string, lineno?: number, colno?: number, error?: Error) {
-        const line = document.createElement('div');
-        line.innerText = `Message: ${message}\nFile name: ${filename}\nLine: ${lineno}\nStack: ${error.stack}`;
-        out.appendChild(line);
-    };
-
 });
