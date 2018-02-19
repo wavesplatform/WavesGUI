7e65bdd80cd68a078940708f4524bde025154984
uiskander <theodsgn@gmail.com>
2018-02-19 16:13:20 +0300

    client-406-browser-support: popup layout

--- src/modules/utils/modals/ModalManager.js
+++ src/modules/utils/modals/ModalManager.js
@@ -215,7 +215,7 @@
                     id: 'token-burn',
                     mod: 'change-token',
                     locals: { money: available, txType: 'reissue' },
-                    titleContent: '<w-i18n params="{name: $ctrl.tx.name}">modal.token.issue.title</w-i18n>',
+                    titleContent: '<w-i18n params="{name: $ctrl.tx.name}">modal.token.reissue.title</w-i18n>',
                     contentUrl: 'modules/utils/modals/changeToken/change-token-modal.html',
                     controller: 'TokenChangeModalCtrl'
                 }));
