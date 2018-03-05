daf066e8f441c2153198413135367232d8f01105
German Tebiev <terbiyarchitect@gmail.com>
2018-03-05 13:21:18 +0300

    CLIENT-549: upgrade market depth chart to handle the lack of asks

--- src/modules/dex/directives/tradeGraph/TradeGraph.js
+++ src/modules/dex/directives/tradeGraph/TradeGraph.js
@@ -1,7 +1,26 @@
 (function () {
     'use strict';
 
-    const ORDERS_STUB = [{ amount: 0, price: 0 }];
+    const SERIES_SETTINGS = {
+        ASKS: {
+            dataset: 'asks',
+            key: 'amount',
+            label: 'Asks',
+            color: '#f27057',
+            type: ['line', 'line', 'area']
+        },
+        BIDS: {
+            dataset: 'bids',
+            key: 'amount',
+            label: 'Bids',
+            color: '#2b9f72',
+            type: ['line', 'line', 'area']
+        }
+    };
+    const ORDER_LIST_STUB = [{ amount: 0, price: 0 }];
+    const THRESHOLD_VALUES = {
+        ASKS: 2
+    };
 
     /**
      * @param Base
@@ -40,20 +59,8 @@
                         y: false
                     },
                     series: [
-                        {
-                            dataset: 'asks',
-                            key: 'amount',
-                            label: 'Asks',
-                            color: '#f27057',
-                            type: ['line', 'line', 'area']
-                        },
-                        {
-                            dataset: 'bids',
-                            key: 'amount',
-                            label: 'Bids',
-                            color: '#2b9f72',
-                            type: ['line', 'line', 'area']
-                        }
+                        SERIES_SETTINGS.ASKS,
+                        SERIES_SETTINGS.BIDS
                     ],
                     axes: {
                         x: { key: 'price', type: 'linear', ticks: 2 },
@@ -62,8 +69,8 @@
                 };
 
                 this.data = {
-                    asks: ORDERS_STUB,
-                    bids: ORDERS_STUB
+                    asks: ORDER_LIST_STUB,
+                    bids: ORDER_LIST_STUB
                 };
 
                 this.syncSettings({
@@ -90,14 +97,23 @@
                 return (
                     waves.matcher
                         .getOrderBook(this._assetIdPair.amount, this._assetIdPair.price)
-                        .then((data) => this._cutOffOutlyingOrders(data))
-                        .then((data) => this._buildCumulativeOrderBook(data))
+                        .then((orderBook) => this._cutOffOutlyingOrdersIfNecessary(orderBook))
+                        .then((orderBook) => this._updateGraphAccordingToOrderBook(orderBook))
+                        .then((orderBook) => this._buildCumulativeOrderBook(orderBook))
                 );
             }
 
             _setOrderBook({ asks, bids }) {
                 this.data.asks = asks;
                 this.data.bids = bids;
+            }
+
+            _cutOffOutlyingOrdersIfNecessary(orderBook) {
+                if (this._areEnoughOrders(orderBook)) {
+                    return this._cutOffOutlyingOrders(orderBook);
+                }
+
+                return orderBook;
             }
 
             _cutOffOutlyingOrders({ asks, bids }) {
@@ -113,6 +129,39 @@
                     asks: asks.filter((ask) => new BigNumber(ask.price).lte(max)),
                     bids: bids.filter((bid) => new BigNumber(bid.price).gte(min))
                 };
+            }
+
+            _updateGraphAccordingToOrderBook(orderBook) {
+                if (this._isLackOfAsks(orderBook)) {
+                    this._setGraphToShowOnlyBids();
+                }
+
+                if (this._areEnoughOrders(orderBook)) {
+                    this._setGraphToShowAsksAndBids();
+                }
+
+                return orderBook;
+            }
+
+            _setGraphToShowOnlyBids() {
+                this.options.series = [
+                    SERIES_SETTINGS.BIDS
+                ];
+            }
+
+            _areEnoughOrders(orderBook) {
+                return !this._isLackOfAsks(orderBook);
+            }
+
+            _isLackOfAsks(orderBook) {
+                return orderBook.asks.length < THRESHOLD_VALUES.ASKS;
+            }
+
+            _setGraphToShowAsksAndBids() {
+                this.options.series = [
+                    SERIES_SETTINGS.ASKS,
+                    SERIES_SETTINGS.BIDS
+                ];
             }
 
             _buildCumulativeOrderBook({ asks, bids }) {
