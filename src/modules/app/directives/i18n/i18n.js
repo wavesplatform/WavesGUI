3134adecccc727dc22f42665c76a9ede2cb4518f
tsigel <tsddaniil@gmail.com>
2018-03-14 12:07:14 +0300

    CLIENT-438: fix localize system

--- src/modules/app/directives/i18n/i18n.js
+++ src/modules/app/directives/i18n/i18n.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    const directive = function (Base, i18n, decorators) {
+    const directive = function (Base, i18n) {
         return {
             scope: true,
             restrict: 'AE',
@@ -12,23 +12,27 @@
                     constructor() {
                         super($scope);
 
-                        this.watchers = Object.create(null);
-                        this.forStop = [];
-                        const handler = this._getHandler();
-                        this.forStop.push(() => i18next.off('languageChanged', handler));
+                        this._forStop = [];
+                        this._watchers = Object.create(null);
+                        this._literalTemplate = I18n._getLiteralTemplate();
+                        this._parts = this._literalTemplate.match(/{{.*?(}})/g);
+                        this._handler = this._getHandler();
 
-                        i18next.on('languageChanged', handler);
+                        this._addWatchers();
+
+                        this.listenEventEmitter(i18next, 'languageChanged', this._handler);
                         if ($attrs.params) {
-                            const stop = $scope.$watch($attrs.params, handler);
-                            this.forStop.push(stop);
+                            const stop = $scope.$watch($attrs.params, this._handler);
+                            this._forStop.push(stop);
                         }
 
-                        handler();
+                        this._handler();
                     }
 
                     $onDestroy() {
                         super.$onDestroy();
-                        this.forStop.forEach((cb) => cb());
+                        this._forStop.forEach((cb) => cb());
+                        this._forStop = [];
                     }
 
                     /**
@@ -36,52 +40,62 @@
                      * @private
                      */
                     _getHandler() {
-                        const literal = this._compile(I18n._getLiteral());
                         const ns = i18n.getNs($element);
-                        return function () {
-                            const skipErros = 'skipErrors' in $attrs;
+                        return () => {
+                            const skipErrors = 'skipErrors' in $attrs;
                             const params = $attrs.params && $scope.$eval($attrs.params) || undefined;
-                            $element.html(i18n.translate(literal, ns, params, skipErros));
+                            $element.html(i18n.translate(this._compile(this._literalTemplate), ns, params, skipErrors));
                         };
                     }
 
                     /**
-                     * @param literal
+                     * @param {string} literal
                      * @return {string}
                      * @private
                      */
                     _compile(literal) {
-                        const parts = literal.match(/{{.*?(}})/g);
-                        let addWatcher = false;
-                        if (!parts) {
-                            return literal.trim();
-                        } else {
-                            parts.forEach((part) => {
-                                if (part.indexOf('::') === -1) {
-                                    addWatcher = true;
-                                }
+                        if (this._parts) {
+                            this._parts.forEach((part) => {
                                 const forEval = part
                                     .replace('{{', '')
                                     .replace('}}', '')
                                     .replace('::', '');
-                                literal = literal.replace(part, $scope.$eval(forEval)).trim();
-                                if (!this.watchers[literal]) {
-                                    this.forStop.push($scope.$watch(forEval, () => this._getHandler()()));
-                                    this.watchers[literal] = true;
-                                }
+
+                                literal = literal.replace(part, $scope.$eval(forEval));
                             });
                         }
-
                         return literal;
+                    }
+
+                    /**
+                     * @private
+                     */
+                    _addWatchers() {
+                        if (!this._parts) {
+                            return null;
+                        }
+
+                        this._parts.forEach((part) => {
+                            const needWatcher = part.indexOf('::') === -1;
+
+                            if (needWatcher && !this._watchers[part]) {
+                                const forEval = part
+                                    .replace('{{', '')
+                                    .replace('}}', '')
+                                    .replace('::', '');
+
+                                this._forStop.push($scope.$watch(forEval, this._handler));
+                                this._watchers[part] = true;
+                            }
+                        });
                     }
 
                     /**
                      * @return {*}
                      * @private
                      */
-                    @decorators.cachable()
-                    static _getLiteral() {
-                        return String(I18n._isAttribute() ? $attrs.wI18n : $element.text()).trim();
+                    static _getLiteralTemplate() {
+                        return String(I18n._isAttribute() ? $element.attr('w-i18n') : $element.text()).trim();
                     }
 
                     /**
@@ -105,7 +119,7 @@
         };
     };
 
-    directive.$inject = ['Base', 'i18n', 'decorators'];
+    directive.$inject = ['Base', 'i18n'];
 
     angular.module('app')
         .directive('wI18n', directive);
