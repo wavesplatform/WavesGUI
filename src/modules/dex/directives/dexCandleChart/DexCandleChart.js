17a7349cdb8a581a426a47a70f2012a842a92a64
tsigel <tsddaniil@gmail.com>
2018-03-15 13:10:44 +0300

    CLIENT-613: remove jslib

--- src/modules/dex/directives/dexCandleChart/DexCandleChart.js
+++ src/modules/dex/directives/dexCandleChart/DexCandleChart.js
@@ -86,7 +86,7 @@
 
     controller.load = function () {
         const script = document.createElement('script');
-        const root = WavesApp.isWeb() ? 'https://jslib.wavesnodes.com/' : '/trading-view/';
+        const root = '/trading-view/';
         script.src = `${root}charting_library.min.js`;
         const promise = new Promise((resolve, reject) => {
             script.onload = resolve;
