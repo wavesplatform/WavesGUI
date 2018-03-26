b3918a4d9e53e691e5777e4690f626d97406f9c1
tsigel <tsddaniil@gmail.com>
2018-03-26 11:48:51 +0300

    CLIENT-685: fix copy module

--- src/modules/ui/directives/copy/copy.js
+++ src/modules/ui/directives/copy/copy.js
@@ -2,54 +2,64 @@
     'use strict';
 
     /**
+     * @param {Base} Base
+     * @param $scope
      * @param {app.utils} utils
      * @param {NotificationManager} notificationManager
+     * @param {JQuery} $element
+     */
+    const controller = function (Base, $scope, utils, notificationManager, $element) {
+
+        class Copy extends Base {
+
+            constructor() {
+                super($scope);
+                const clipboard = new Clipboard($element.get(0), {
+                    text: () => $scope.wCopy,
+                    container: $element.get(0)
+                });
+
+                clipboard.on('success', () => {
+                    notificationManager.info({
+                        ns: 'app.ui',
+                        title: { literal: 'copySuccess' }
+                    });
+                });
+
+                clipboard.on('error', () => {
+                    notificationManager.error({
+                        ns: 'app.ui',
+                        title: { literal: 'copyError' }
+                    });
+                });
+
+                this.signals.destroy.once(() => {
+                    clipboard.destroy();
+                });
+            }
+
+        }
+
+        return new Copy();
+    };
+
+    controller.$inject = ['Base', '$scope', 'utils', 'notificationManager', '$element'];
+
+    /**
      * @return {*}
      */
-    const directive = function (utils, notificationManager) {
+    const directive = function () {
         return {
             restrict: 'A',
             priority: 100,
-            scope: false,
-            compile: function ($element, $attrs) {
-                return function ($scope, $element) {
-
-                    const destroy = new tsUtils.Signal();
-                    const stop = $scope.$on('$destroy', () => {
-                        destroy.dispatch();
-                        stop();
-                    });
-
-                    const param = utils.parseAngularParam($attrs.wCopy, $scope, destroy);
-                    const clipboard = new Clipboard($element.get(0), {
-                        text: () => param.value,
-                        container: $element.get(0)
-                    });
-
-                    clipboard.on('success', () => {
-                        notificationManager.info({
-                            ns: 'app.ui',
-                            title: { literal: 'copySuccess' }
-                        });
-                    });
-
-                    clipboard.on('error', () => {
-                        notificationManager.error({
-                            ns: 'app.ui',
-                            title: { literal: 'copyError' }
-                        });
-                    });
-
-                    destroy.once(() => {
-                        clipboard.destroy();
-                    });
-
-                };
-            }
+            scope: {
+                wCopy: '<'
+            },
+            controller
         };
     };
 
-    directive.$inject = ['utils', 'notificationManager'];
+    directive.$inject = [];
 
     angular.module('app.ui').directive('wCopy', directive);
 })();
--- src/modules/ui/directives/copyWrap/CopyWrap.js
+++ src/modules/ui/directives/copyWrap/CopyWrap.js
@@ -5,7 +5,7 @@
         bindings: {
             info: '<'
         },
-        template: '<div class="container" ng-transclude></div><div w-copy="{{$ctrl.info}}" class="copy-icon"></div>',
-        transclude: true
+        template: '<div class="container" ng-transclude></div><div w-copy="$ctrl.info" class="copy-icon"></div>',
+        transclude: true,
     });
 })();
