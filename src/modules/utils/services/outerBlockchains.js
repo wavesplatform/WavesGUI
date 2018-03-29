a09ee9a709038b7a916ac7c1419b194e2d00fad9
xenohunter <xenohunter@yandex.ru>
2018-03-29 12:11:59 +0300

    CLIENT-695: added new BCH address format in the regex

--- src/modules/utils/services/outerBlockchains.js
+++ src/modules/utils/services/outerBlockchains.js
@@ -6,7 +6,7 @@
         [WavesApp.defaultAssets.ETH]: /^0x[0-9a-f]{40}$/i,
         [WavesApp.defaultAssets.LTC]: /^[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}$/,
         [WavesApp.defaultAssets.ZEC]: /^t[0-9a-z]{34}$/i,
-        [WavesApp.defaultAssets.BCH]: /[a-km-zA-HJ-NP-Z1-9]{25,34}$/
+        [WavesApp.defaultAssets.BCH]: /^([13][a-km-zA-HJ-NP-Z1-9]{25,34}|[qp][a-zA-Z0-9]{41})$/
     };
 
     const factory = function () {
