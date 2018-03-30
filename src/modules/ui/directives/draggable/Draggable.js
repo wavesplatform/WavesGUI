61de2e7aa43f084b38ab5da4db8c8cb6c24d645f
tsigel <tsddaniil@gmail.com>
2018-03-30 18:35:44 +0300

    CLIENT-255: refactor Draggable

--- src/modules/ui/directives/draggable/Draggable.js
+++ src/modules/ui/directives/draggable/Draggable.js
@@ -1,11 +1,6 @@
 (function () {
     'use strict';
 
-    const PAGE_COORDINATES = {
-        x: 'pageX',
-        y: 'pageY'
-    };
-
     const MOUSE_EVENTS = {
         DOWN: 'mousedown',
         MOVE: 'mousemove',
@@ -28,217 +23,228 @@
 
     /**
      * @param {app.utils} utils
-     * @param $document
-     * @return {{scope: boolean, link: function(*, *=)}}
+     * @param {JQuery} $document
+     * @param {Base} Base
+     * @return {*}
      */
-    const directive = (utils, $document) => {
+    const directive = (utils, $document, Base) => {
 
         return {
             scope: false,
             link: ($scope, $element) => {
 
-                const $window = $(window);
-                const $overlay = $('<div class="drag-overlay">');
+                class Draggable extends Base {
 
-                let translation = {
-                    x: 0,
-                    y: 0
-                };
+                    constructor() {
+                        super($scope);
 
-                const initialOffset = {
-                    x: 0,
-                    y: 0
-                };
+                        const startRect = $element.get(0).getBoundingClientRect();
 
-                let bounds = {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                };
+                        this.$overlay = $('<div class="drag-overlay">');
+                        this.delta = { x: 0, y: 0 };
+                        this.translation = { x: startRect.left, y: startRect.top };
+                        this.id = 0;
+                        this.moveHandler = this._getMoveHandler();
+                        this.resizeHandler = utils.debounceRequestAnimationFrame(() => this._onResize());
 
-                let id = 0;
+                        this._initializeElementPosition();
+                        this._setHandlers();
+                        this._draw();
 
-                init();
-
-                function init() {
-                    const position = $element.css('position');
-                    if (position === 'static' || position === 'relative') {
-                        const offset = $element.offset();
-                        $element.css({
-                            position: 'absolute',
-                            transformOrigin: 'left top',
-                            willChange: 'transform'
-                        });
-                        $element.offset(offset);
                     }
 
-                    $element.css('zIndex', 10);
-                    document.body.appendChild($element.get(0));
-
-                    updateCoordinatesAndInscribeElement();
-
-                    const stop = $scope.$on('$destroy', () => {
-                        stop();
+                    $onDestroy() {
+                        super.$onDestroy();
                         $element.remove();
-                        $overlay.remove();
-                    });
+                    }
 
-                    $scope.$watch(() => $element.width(), updateBoundsAndInscribeElement);
-                    $scope.$watch(() => $element.height(), updateBoundsAndInscribeElement);
-                    $window.resize(updateCoordinatesAndInscribeElement);
-                    $document.scroll(updateCoordinates);
-
-                    $element.on(POINTER_EVENTS.START, (startEvent) => {
-                        $overlay.insertBefore($element);
-
-                        startEvent = getDistilledEvent(startEvent);
-                        if (startEvent.button === MAIN_MOUSE_BUTTON) {
-                            const onMove = getMoveHandler(startEvent);
-                            $document.on(POINTER_EVENTS.MOVE, onMove);
-                            $document.one(POINTER_EVENTS.END, getPointerEndHandler(onMove));
+                    /**
+                     * @private
+                     */
+                    _initializeElementPosition() {
+                        const position = $element.css('position');
+                        if (position === 'static' || position === 'relative') {
+                            $element.css('position', 'absolute');
                         }
-                    });
-                }
-
-                function updateCoordinatesAndInscribeElement() {
-                    updateCoordinates();
-                    inscribeElementInBounds();
-                }
-
-                function updateCoordinates() {
-                    updateInitialOffset();
-                    updateBounds();
-                }
-
-                function updateBoundsAndInscribeElement() {
-                    updateBounds();
-                    inscribeElementInBounds();
-                }
-
-                function updateInitialOffset() {
-                    initialOffset.x = $element.offset().left - translation.x;
-                    initialOffset.y = $element.offset().top - translation.y;
-                }
-
-                function updateBounds() {
-                    const scroll = {
-                        x: $window.scrollLeft(),
-                        y: $window.scrollTop()
-                    };
-
-                    bounds = {
-                        left: scroll.x - initialOffset.x,
-                        right: scroll.x + window.innerWidth - (initialOffset.x + $element.width()),
-                        top: scroll.y - initialOffset.y,
-                        bottom: scroll.y + window.innerHeight - (initialOffset.y + $element.height())
-                    };
-                }
-
-                function getMoveHandler(startEvent) {
-                    const setTranslation = buildTranslationSetter(startEvent);
-
-                    return (moveEvent) => {
-                        moveEvent.preventDefault();
-                        setTranslation(moveEvent);
-                    };
-                }
-
-                function buildTranslationSetter(startEvent) {
-                    const start = calculateRelativeCoordinates(startEvent, translation);
-
-                    return utils.debounceRequestAnimationFrame((moveEvent) => {
-                        moveEvent = getDistilledEvent(moveEvent);
-                        translation = calculateRelativeCoordinates(moveEvent, start);
-                        inscribeElementInBounds();
-                    });
-                }
-
-                function calculateRelativeCoordinates(event, basicCoordinates) {
-                    const relativeCoordinates = {};
-
-                    Object.keys(PAGE_COORDINATES).forEach((coordinate) => {
-                        const eventCoordinate = event[PAGE_COORDINATES[coordinate]];
-                        relativeCoordinates[coordinate] = eventCoordinate - basicCoordinates[coordinate];
-                    });
-
-                    return relativeCoordinates;
-                }
-
-                function getDistilledEvent(event) {
-                    const touch = getTouch(event.originalEvent);
-                    const distilledEvent = {
-                        preventDefault: () => event.originalEvent.preventDefault(),
-                        button: event.button || MAIN_MOUSE_BUTTON
-                    };
-
-                    Object.values(PAGE_COORDINATES).forEach((coordinate) => {
-                        distilledEvent[coordinate] = getCoordinateValue(event, touch, coordinate);
-                    });
-
-                    return distilledEvent;
-                }
-
-                /**
-                 * @param {TouchEvent} event
-                 */
-                function getTouch(event) {
-                    switch (event.type) {
-                        case TOUCH_EVENTS.START:
-                            id = event.targetTouches[0].identifier;
-                            return getTouchEventById(event);
-                        case TOUCH_EVENTS.MOVE:
-                            return getTouchEventById(event);
-                        case TOUCH_EVENTS.END:
-                            id = 0;
-                            return event.changedTouches[0];
-                        default:
-                            return {};
-                    }
-                }
-
-                function getTouchEventById(event) {
-                    return Array.prototype.filter.call(event.targetTouches, ({ identifier }) => identifier === id)[0];
-                }
-
-                function getCoordinateValue(event, touch, coordinate) {
-                    if (typeof event[coordinate] !== 'number') {
-                        return touch[coordinate];
+                        $element.css({
+                            transformOrigin: 'left top',
+                            willChange: 'transform',
+                            zIndex: 40,
+                            left: 0,
+                            top: 0
+                        });
+                        document.body.appendChild($element.get(0));
                     }
 
-                    return event[coordinate];
+                    /**
+                     * @private
+                     */
+                    _setHandlers() {
+                        this.listenEventEmitter($element, POINTER_EVENTS.START, this._onStart.bind(this));
+                        this.listenEventEmitter($(window), 'resize', this.resizeHandler);
+                        $scope.$watch(Draggable._getResizeWatchCb(), this.resizeHandler);
+                    }
+
+                    /**
+                     * @param {MouseEvent|TouchEvent} e
+                     * @private
+                     */
+                    _onStart(e) {
+                        const event = this._getDistilledEvent(e);
+                        event.preventDefault();
+
+                        if (event.button !== MAIN_MOUSE_BUTTON) {
+                            return false;
+                        }
+
+                        this.$overlay.insertBefore($element);
+
+                        const rect = $element.get(0).getBoundingClientRect();
+                        this.delta.x = e.pageX - rect.left;
+                        this.delta.y = e.pageY - rect.top;
+
+                        this.listenEventEmitter($document, POINTER_EVENTS.MOVE, this.moveHandler);
+                        this.listenEventEmitter($document, POINTER_EVENTS.END, this._onEnd.bind(this));
+                    }
+
+                    /**
+                     * @param {IDistilledEvent} e
+                     * @private
+                     */
+                    _onMove(e) {
+                        const rect = $element.get(0).getBoundingClientRect();
+                        const x = e.pageX - this.delta.x;
+                        const y = e.pageY - this.delta.y;
+
+                        this._updateTranslation(rect, { x, y });
+                        this._draw();
+                    }
+
+                    /**
+                     * @private
+                     */
+                    _onResize() {
+                        const rect = $element.get(0).getBoundingClientRect();
+
+                        this._updateTranslation(rect);
+                        this._draw();
+                    }
+
+                    /**
+                     * @private
+                     */
+                    _onEnd() {
+                        this.stopListenEventEmitter(POINTER_EVENTS.MOVE, $document);
+                        this.stopListenEventEmitter(POINTER_EVENTS.END, $document);
+                    }
+
+                    /**
+                     * @param {ClientRect} rect
+                     * @param {{x: number, y: number}} [coordinates]
+                     * @private
+                     */
+                    _updateTranslation(rect, coordinates = { x: rect.left, y: rect.top }) {
+                        this.translation.x = Math.min(Math.max(coordinates.x, 0), innerWidth - rect.width);
+                        this.translation.y = Math.min(Math.max(coordinates.y, 0), innerHeight - rect.height);
+                    }
+
+                    /**
+                     * @param {MouseEvent|TouchEvent} e
+                     * @return {IDistilledEvent}
+                     * @private
+                     */
+                    _getDistilledEvent(e) {
+                        const touch = this._getTouch(e.originalEvent);
+                        return {
+                            preventDefault: () => e.originalEvent.preventDefault(),
+                            button: e.button || MAIN_MOUSE_BUTTON,
+                            pageX: Draggable._getCoordinateValue(e, touch, 'pageX'),
+                            pageY: Draggable._getCoordinateValue(e, touch, 'pageY')
+                        };
+                    }
+
+                    /**
+                     * @return {function}
+                     * @private
+                     */
+                    _getMoveHandler() {
+                        return utils.debounceRequestAnimationFrame((e) => {
+                            return this._onMove(this._getDistilledEvent(e));
+                        });
+                    }
+
+                    /**
+                     * @param {TouchEvent} event
+                     * @return {*}
+                     * @private
+                     */
+                    _getTouch(event) {
+                        switch (event.type) {
+                            case TOUCH_EVENTS.START:
+                                this.id = event.targetTouches[0].identifier;
+                                return this._getTouchEventById(event);
+                            case TOUCH_EVENTS.MOVE:
+                                return this._getTouchEventById(event);
+                            case TOUCH_EVENTS.END:
+                                this.id = 0;
+                                return event.changedTouches[0];
+                            default:
+                                return {};
+                        }
+                    }
+
+                    /**
+                     * @param event
+                     * @return {Touch}
+                     * @private
+                     */
+                    _getTouchEventById(event) {
+                        return Array.prototype.filter
+                            .call(event.targetTouches, ({ identifier }) => identifier === this.id)[0];
+                    }
+
+                    /**
+                     * @private
+                     */
+                    _draw() {
+                        $element.css('transform', `translate(${this.translation.x}px, ${this.translation.y}px)`);
+                    }
+
+                    static _getResizeWatchCb() {
+                        /**
+                         * @type {HTMLElement}
+                         */
+                        const element = $element.get(0);
+                        return () => `${element.clientWidth} / ${element.clientHeight}`;
+                    }
+
+                    static _getCoordinateValue(event, touch, coordinate) {
+                        if (typeof event[coordinate] !== 'number') {
+                            return touch[coordinate];
+                        }
+
+                        return event[coordinate];
+                    }
+
                 }
 
-                function inscribeElementInBounds() {
-                    translation.x = getBoundedValue(bounds.left, translation.x, bounds.right);
-                    translation.y = getBoundedValue(bounds.top, translation.y, bounds.bottom);
-                    $element.css('transform', `translate(${translation.x}px, ${translation.y}px)`);
-                }
-
-                function getBoundedValue(lowerBound, value, upperBound) {
-                    return Math.min(
-                        Math.max(
-                            lowerBound,
-                            value
-                        ),
-                        upperBound
-                    );
-                }
-
-                function getPointerEndHandler(onMove) {
-                    return () => {
-                        $overlay.detach();
-                        $document.off(POINTER_EVENTS.MOVE, onMove);
-                    };
-                }
+                new Draggable();
 
             }
         };
     };
 
-    directive.$inject = ['utils', '$document'];
+    directive.$inject = ['utils', '$document', 'Base'];
 
     angular.module('app.ui').directive('wDraggable', directive);
 
 })();
+
+/**
+ * @typedef {object} IDistilledEvent
+ * @property {Function } preventDefault
+ * @property {number} button
+ * @property {number} pageX
+ * @property {number} pageY
+ */
 
