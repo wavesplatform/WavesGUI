5b61d510430918ccb51bab58ceb13d832e66e241
Xema <xema888@hotmail.es>
2018-03-31 07:06:59 +0200

    Update AssetLogo.js

--- src/modules/ui/directives/assetLogo/AssetLogo.js
+++ src/modules/ui/directives/assetLogo/AssetLogo.js
@@ -11,7 +11,7 @@
         [WavesApp.defaultAssets.EUR]: '/img/assets/euro.svg',
         [WavesApp.defaultAssets.USD]: '/img/assets/usd.svg',
         [WavesApp.defaultAssets.BCH]: '/img/assets/bitcoin-cash.svg',
-        [WavesApp.otherAssetsWithIcons.DART]: '/img/assets/efyt.svg',
+        [WavesApp.otherAssetsWithIcons.DART]: '/img/assets/Darextravel.svg',
         [WavesApp.otherAssetsWithIcons.EFYT]: '/img/assets/efyt.svg',
         [WavesApp.otherAssetsWithIcons.WNET]: '/img/assets/wnet.svg'
     };
