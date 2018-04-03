5ebbf48030b6e24883f4fdc19cb2f5d99ed69371
beregovoy68 <s.a.ishchenko@gmail.com>
2018-04-04 00:11:18 +1000

    CLIENT-575: Changed nodes and matcher domain to wavesplatform.com

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,9 +98,9 @@ module.exports = function (grunt) {
                 mainnet: {
                     name: 'mainnet',
                     code: 'W',
-                    server: 'https://nodes.wavesnodes.com',
+                    server: 'https://nodes.wavesplatform.com',
                     coinomat: 'https://coinomat.com',
-                    matcher: 'https://matcher.wavesnodes.com',
+                    matcher: 'https://matcher.wavesplatform.com',
                     datafeed: 'https://marketdata.wavesplatform.com'
                 },
                 devnet: {
