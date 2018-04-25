783e7d2807f1c04ae2e11b7144e3747c8ca0219a
tsigel <tsddaniil@gmail.com>
2018-04-25 16:52:45 +0300

    CLIENT-830: fix open modals

--- src/modules/ui/directives/confirmTransaction/ConfirmTransaction.js
+++ src/modules/ui/directives/confirmTransaction/ConfirmTransaction.js
@@ -10,9 +10,11 @@
      * @param {ModalManager} modalManager
      * @param {User} user
      * @param {$rootScope.Scope} $scope
+     * @param {app.utils} utils
+     * @param {ValidateService} validateService
      * @returns {ConfirmTransaction}
      */
-    const controller = function (Base, waves, $attrs, $mdDialog, modalManager, user, $scope) {
+    const controller = function (Base, waves, $attrs, $mdDialog, modalManager, user, $scope, utils, validateService) {
 
         const TYPES = WavesApp.TRANSACTION_TYPES.NODE;
 
@@ -32,6 +34,16 @@
                  * @type {number}
                  */
                 this.step = 0;
+                /**
+                 * @type {boolean}
+                 */
+                this.showValidationErrors = false;
+                /**
+                 * @type {Array}
+                 */
+                this.errors = [];
+
+                this.observe('showValidationErrors', this._showErrors);
             }
 
             confirm() {
@@ -39,7 +51,7 @@
                     this.tx.id = id;
                     this.step++;
                     this.onTxSent({ id });
-                    $scope.$digest();
+                    $scope.$apply();
                 }).catch((e) => {
                     console.error(e);
                     console.error('Transaction error!');
@@ -107,6 +119,43 @@
             }
 
             /**
+             * @private
+             */
+            _showErrors() {
+                if (this.showValidationErrors) {
+                    if (this.tx.transactionType === TYPES.TRANSFER) {
+                        const errors = [];
+                        Promise.all([
+                            waves.node.assets.userBalances()
+                                .then((list) => list.map(({ available }) => available))
+                                .then((list) => {
+                                    const hash = utils.toHash(list, 'asset.id');
+                                    const amount = this.tx.amount;
+                                    if (!hash[amount.asset.id] || hash[amount.asset.id].lt(amount)) {
+                                        errors.push({
+                                            literal: 'no balance'
+                                        });
+                                    }
+                                }),
+                            utils.resolve(utils.when(validateService.wavesAddress(this.tx.recipient)))
+                                .then(({ state }) => {
+                                    if (!state) {
+                                        errors.push({
+                                            literal: 'wrong address'
+                                        });
+                                    }
+                                })
+                        ]).then(() => {
+                            this.errors = errors;
+                            $scope.$apply();
+                        });
+                    }
+                } else {
+                    this.errors = [];
+                }
+            }
+
+            /**
              * @param {string} str
              * @returns {string}
              */
@@ -123,14 +172,25 @@
         return new ConfirmTransaction();
     };
 
-    controller.$inject = ['Base', 'waves', '$attrs', '$mdDialog', 'modalManager', 'user', '$scope'];
+    controller.$inject = [
+        'Base',
+        'waves',
+        '$attrs',
+        '$mdDialog',
+        'modalManager',
+        'user',
+        '$scope',
+        'utils',
+        'validateService'
+    ];
 
     angular.module('app.ui').component('wConfirmTransaction', {
         bindings: {
             tx: '<',
             onClickBack: '&',
             onTxSent: '&',
-            noBackButton: '<'
+            noBackButton: '<',
+            showValidationErrors: '<'
         },
         templateUrl: 'modules/ui/directives/confirmTransaction/confirmTransaction.html',
         transclude: false,
