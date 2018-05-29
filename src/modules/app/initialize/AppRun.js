997520eb185b6f809164b2c3dd3825998adeacc2
tsigel <tsddaniil@gmail.com>
2018-05-29 14:54:00 +0300

    CLIENT-907: add origin github link

--- src/modules/app/initialize/AppRun.js
+++ src/modules/app/initialize/AppRun.js
@@ -61,9 +61,13 @@
             });
         });
 
+
+        const proxy = 'https://github-proxy.wvservices.com/wavesplatform/WavesGUI/client-907-fix-portfolio/scam.csv';
+        const origin = 'https://raw.githubusercontent.com/wavesplatform/WavesGUI/client-907-fix-portfolio/scam.csv';
         const papa = require('papaparse');
 
-        fetch('https://github-proxy.wvservices.com/wavesplatform/WavesGUI/client-907-fix-portfolio/scam.csv')
+        fetch(proxy)
+            .catch(() => fetch(origin))
             .then((text) => {
                 papa.parse(text).data.forEach(([id]) => {
                     WavesApp.scam[id] = true;
