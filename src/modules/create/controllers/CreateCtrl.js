66737175c082e0bef5081c5a403306a259fe8b01
Boris Volkov <vba@mail.ru>
2018-06-13 17:00:45 +0300

    CLIENT-890: fix create user no save

--- src/modules/create/controllers/CreateCtrl.js
+++ src/modules/create/controllers/CreateCtrl.js
@@ -134,9 +134,12 @@
             }
 
             _create(hasBackup) {
+                if (!this.saveUserData) {
+                    this.password = Date.now().toString();
+                }
 
                 const seedData = Waves.Seed.fromExistingPhrase(this.seed);
-                const encryptedSeed = this.saveUserData ? seedData.encrypt(this.password) : '';
+                const encryptedSeed = seedData.encrypt(this.password);
                 const publicKey = seedData.keyPair.publicKey;
 
                 return user.create({
