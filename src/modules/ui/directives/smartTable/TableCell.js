6dcca9a509e7bf1abd67df467ccfb901ffa1cb80
tsigel <tsddaniil@gmail.com>
2018-06-18 18:24:27 +0300

    CLIENT-872: fix table for style

--- src/modules/ui/directives/smartTable/TableCell.js
+++ src/modules/ui/directives/smartTable/TableCell.js
@@ -1,12 +1,10 @@
 (function () {
     'use strict';
 
-    /**
-     * @param {STService} stService
-     * @return {*}
-     */
-    const directive = (stService) => ({
-        bindings: {},
+    const directive = () => ({
+        require: {
+            parent: '?^wTableRow'
+        },
         replace: true,
         template: '<div class="smart-table__cell" ng-transclude></div>',
         transclude: true,
@@ -15,22 +13,17 @@
          * @param {JQuery} $element
          */
         link: {
-            post: function ($scope, $element) {
-                const parentCid = $element.closest('[data-cid]').attr('data-cid');
-                const element = $element.get(0);
-                const index = $element.index();
+            post: function ($scope, $element, _, { parent }) {
                 const idAttrName = 'data-column-id';
 
-                if (element.parentElement) {
-                    $element.attr(idAttrName, stService.getTableByCid(parentCid).getIdByIndex(index));
+                if (parent) {
+                    parent.registerCell($element, $scope.id);
                 } else {
                     $element.attr(idAttrName, $scope.id);
                 }
             }
         }
     });
-
-    directive.$inject = ['stService'];
 
     angular.module('app.ui').directive('wTableCell', directive);
 })();
