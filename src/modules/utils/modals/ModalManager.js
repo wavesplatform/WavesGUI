0823edd6a953dae6aa77943cfb8feafea1d283a4
uiskander <theodsgn@gmail.com>
2018-07-18 18:43:23 +0300

    CLIENT-1082: add asset layout ready

--- src/modules/utils/modals/ModalManager.js
+++ src/modules/utils/modals/ModalManager.js
@@ -72,7 +72,7 @@
                 return this._getModal({
                     id: 'pin-asset',
                     title: 'modal.pinAsset.title',
-                    contentUrl: 'modules/utils/modals/pinAsset/pinAsset.html',
+                    templateUrl: 'modules/utils/modals/pinAsset/pinAsset.html',
                     controller: 'PinAssetCtrl',
                     mod: 'pin-asset-modal'
                 });
