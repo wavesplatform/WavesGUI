80996aa3ee70a6b892cfe205f37fd91e60e49347
vba2000 <vba@mail.ru>
2018-07-21 09:37:03 +0300

    CLIENT-794: fix pending

--- src/modules/utils/modals/accountInfo/AccountInfoCtrl.js
+++ src/modules/utils/modals/accountInfo/AccountInfoCtrl.js
@@ -141,11 +141,12 @@
                 this.invalidMinLength = this.newAlias && this.newAlias.length < MIN_ALIAS_LENGTH;
                 this.invalidMaxLength = this.newAlias && this.newAlias.length > MAX_ALIAS_LENGTH;
                 this.invalidPattern = this.newAlias && !ALIAS_PATTERN.test(this.newAlias);
-                this.invalid = this.noMoneyForFee || this.invalidMinLength ||
+                const invalid = this.noMoneyForFee || this.invalidMinLength ||
                 this.invalidMaxLength || this.invalidPattern;
 
-                if (this.newAlias && !this.invalid) {
+                if (this.newAlias && !invalid) {
                     this.pendingAlias = true;
+                    this.invalid = true;
                     try {
                         await ds.api.aliases.getAddressByAlias(this.newAlias);
                         this.invalidExist = true;
@@ -154,8 +155,10 @@
                     }
 
                     this.pendingAlias = false;
-                    this.invalid = this.invalid || this.invalidExist;
+                    this.invalid = invalid || this.invalidExist;
                     $scope.$digest();
+                } else {
+                    this.invalid = invalid;
                 }
             }
 
