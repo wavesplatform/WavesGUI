448c83da2e89d460886f2739d45bfe6fdde84b9e
vba2000 <vba@mail.ru>
2018-09-12 14:57:19 +0300

    CLIENT-1181: fix fee

--- src/modules/ui/directives/feeList/FeeList.js
+++ src/modules/ui/directives/feeList/FeeList.js
@@ -41,8 +41,9 @@
                 }
 
                 waves.node.getFeeList({ type: this.type }).then(list => {
-                    this.feeList = list;
-                    if (!(this.fee && find(list, item => item.asset.id === this.fee.asset.id))) {
+                    this.originalFeeList = list;
+                    this._setActualFeeList();
+                    if (!(this.fee && find(this.feeList, item => item.asset.id === this.fee.asset.id))) {
                         const fee = this.balanceHash && Object.keys(this.balanceHash).length && list.find(item => {
                             const balance = this.balanceHash[item.asset.id];
                             return balance && balance.gte(item);
@@ -58,17 +59,41 @@
                     return null;
                 }
 
+                this._setActualFeeList();
+
                 if (this.feeList.length === 1) {
                     return null;
                 }
 
                 const feeBalance = this.balanceHash[this.fee.asset.id];
+
                 if (!feeBalance || feeBalance.lt(this.fee)) {
                     this.fee = this.balanceHash && Object.keys(this.balanceHash).length && this.feeList.find(item => {
                         const balance = this.balanceHash[item.asset.id];
                         return balance && balance.gte(item);
                     }) || this.fee;
                 }
+            }
+
+            _setActualFeeList(list = this.originalFeeList) {
+                const hasBalances = this.balanceHash && Object.keys(this.balanceHash).length;
+
+                if (!list || list.length < 2 || !hasBalances) {
+                    this.feeList = [...list];
+                    return null;
+                }
+
+                const wavesFee = list.find(item => item.asset.id === 'WAVES');
+                const filteredList = list.filter((fee) => {
+                    const feeBalance = this.balanceHash[fee.asset.id];
+                    return !(!hasBalances || !feeBalance || feeBalance.lt(fee));
+                });
+
+                if (!filteredList.length) {
+                    filteredList.push(wavesFee);
+                }
+
+                this.feeList = filteredList;
             }
 
         }
