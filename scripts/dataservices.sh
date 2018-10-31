d64e3b68c95391c27fd7349d557eea1e4bd09dd4
vba2000 <vba@mail.ru>
2018-10-31 17:05:56 +0300

    CLIENT-DEV: fix 2

--- scripts/dataservices.sh
+++ scripts/dataservices.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./node_modules/.bin/tsc -p ./data-service  || exit 1
 echo "compile data services >> DONE"
-./node_modules/.bin/browserify data-service/index.js --node -s ds -u @waves/assets-pairs-order -u ts-utils -u @waves/data-entities -u @waves/signature-generator -u @waves/signature-adapter -u ramda --no-bf -o ./data-service-dist/data-service.js || exit 1
+./node_modules/.bin/browserify data-service/index.js --node -s ds -u @waves/assets-pairs-order -u ts-utils -u @waves/data-entities -u @waves/signature-generator -u @waves/signature-adapter -u ramda --no-bf -o ./data-service-dist/data-service.js
 echo "Build data services >> DONE"
 exit 0
