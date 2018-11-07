e7cab0f9fd57b8be37f8104a0afd54f8b143eadd
tsigel <tsddaniil@gmail.com>
2018-11-07 14:10:10 +0300

    CLIENT-1444: fix advanced mode

--- src/modules/utils/modals/settings/SettingsCtrl.js
+++ src/modules/utils/modals/settings/SettingsCtrl.js
@@ -36,7 +36,7 @@
             }
 
             set advancedMode(mode) {
-                analytics.push('Settings', 'Settings.ChangeAdvancedMode', String(!mode));
+                analytics.push('Settings', 'Settings.ChangeAdvancedMode', String(mode));
                 user.setSetting('advancedMode', mode);
             }
 
