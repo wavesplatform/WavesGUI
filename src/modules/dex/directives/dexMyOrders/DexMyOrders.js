a7a4e28af1d928ff566e877aeb89ed52d4467bac
electricjesus95 <44834528+electricjesus95@users.noreply.github.com>
2018-11-13 12:22:43 +0300

    CLIENT-1468: catch ds error

--- src/modules/dex/directives/dexMyOrders/DexMyOrders.js
+++ src/modules/dex/directives/dexMyOrders/DexMyOrders.js
@@ -333,7 +333,7 @@
                                 order.exchange = transactionsByOrderHash[order.id];
                                 return order;
                             });
-                        });
+                        }).catch( () => result);
                     })
                     .catch(() => {
                         this.loadingError = true;
