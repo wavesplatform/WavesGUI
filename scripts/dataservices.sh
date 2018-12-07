f1bbaa0dd6cbe930a2d41659e1383fd5db111057
tsigel <tsddaniil@gmail.com>
2018-12-07 13:18:14 +0300

    CLIENT-1557: remove build mode "node"

--- scripts/dataservices.sh
+++ scripts/dataservices.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ./node_modules/.bin/tsc -p ./data-service  || exit 1
 echo "compile data services >> DONE"
-./node_modules/.bin/browserify data-service/index.js --node -s ds -u ts-utils -u @waves/data-entities -u @waves/signature-generator -u @waves/signature-adapter -u ramda --no-bf -o ./data-service-dist/data-service-es6.js
+./node_modules/.bin/browserify data-service/index.js -s ds -u ts-utils -u @waves/data-entities -u @waves/signature-generator -u @waves/signature-adapter -u ramda --no-bf -o ./data-service-dist/data-service-es6.js
 ./node_modules/.bin/babel data-service-dist/data-service-es6.js --presets=es2015 --plugins=transform-decorators-legacy,transform-class-properties,transform-object-rest-spread > data-service-dist/data-service.js
 echo "Build data services >> DONE"
 exit 0
