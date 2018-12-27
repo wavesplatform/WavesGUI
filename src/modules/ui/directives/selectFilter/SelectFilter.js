91c40b64c0d15182a5b566784b0aeb62abaaae7e
uiskander <theodsgn@gmail.com>
2018-12-27 19:27:13 +0300

    CLIENT-1566: add selectFilter component

---
+++ src/modules/ui/directives/selectFilter/SelectFilter.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    angular.module('app.ui').component('wSelectFilter', {
+        templateUrl: 'modules/ui/directives/selectFilter/selectFilter.html',
+        transclude: true
+    });
+})();
