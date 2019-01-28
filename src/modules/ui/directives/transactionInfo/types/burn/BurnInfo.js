0dddb1b6c27b705764a4a1b81b535d7aad9d0369
tsigel <tsddaniil@gmail.com>
2019-01-28 15:34:04 +0300

    CLIENT-1614: fix burn

--- src/modules/ui/directives/transactionInfo/types/burn/BurnInfo.js
+++ src/modules/ui/directives/transactionInfo/types/burn/BurnInfo.js
@@ -72,8 +72,8 @@
                     const quantity = new Money(asset.quantity, asset);
                     const toReissue = this.transaction.amount;
 
-                    this.toRemainOnBalance = (hash[assetId] || new Money(0, asset)).add(toReissue);
-                    this.totalAfterIssueTokens = quantity.add(toReissue);
+                    this.toRemainOnBalance = (hash[assetId] || new Money(0, asset)).minus(toReissue);
+                    this.totalAfterIssueTokens = quantity.minus(toReissue);
 
                     $scope.$apply();
                 });
