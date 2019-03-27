9ed2e42ba969272deb6347b6665640c90b84b99a
Alexand Andreev <aandreev@wavesplatform.com>
2019-03-27 12:37:08 +0300

    CLIENT-1783: fix leasing info

--- src/modules/ui/directives/transactionInfo/TransactionInfoContent.js
+++ src/modules/ui/directives/transactionInfo/TransactionInfoContent.js
@@ -3,7 +3,8 @@
 
     angular.module('app.ui').component('wTransactionInfoContent', {
         bindings: {
-            signable: '<'
+            signable: '<',
+            isConfirm: '<'
         },
         templateUrl: 'modules/ui/directives/transactionInfo/transaction-info-content.html'
     });
