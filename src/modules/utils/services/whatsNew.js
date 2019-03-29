b477cf0e897e1c8f5d5a471c3192597b4e9d14ac
Max Axenov <maksenov@wavesplatform.com>
2019-03-29 18:44:18 +0300

    CLIENT-1246: edit addversions logic

--- src/modules/utils/services/whatsNew.js
+++ src/modules/utils/services/whatsNew.js
@@ -6,9 +6,8 @@
      * @param {INotification} notification
      * @param {User} user
      * @param {Migration} migration
-     * @param {app.utils} utils
      */
-    const factory = function (notification, user, migration, utils) {
+    const factory = function (notification, user, migration) {
 
         const MIGRATION_LIST = [
             '1.0.0',
@@ -39,16 +38,16 @@
             '1.2.0'
         ];
 
-        /**
-         * @param {string|Array<string>} version
-         */
-        function addVersions(version) {
-            const versions = user.getSetting('whatsNewList').slice();
-            utils.toArray(version).forEach((version) => {
-                versions.push(version);
-            });
-            user.setSetting('whatsNewList', versions);
-        }
+        // /**
+        //  * @param {string|Array<string>} version
+        //  */
+        // function addVersions(version) {
+        //     const versions = user.getSetting('whatsNewList').slice();
+        //     utils.toArray(version).forEach((version) => {
+        //         versions.push(version);
+        //     });
+        //     user.setSetting('whatsNewList', versions);
+        // }
 
         function removeVersion(version) {
             const versions = user.getSetting('whatsNewList').slice();
@@ -71,7 +70,6 @@
             const notShownUpdates = user.getSetting('whatsNewList');
             const lastOpenVersion = user.getSetting('lastOpenVersion');
             const newVersionList = migration.migrateFrom(lastOpenVersion || WavesApp.version, MIGRATION_LIST);
-
             migration.sort(unique(newVersionList, notShownUpdates)).forEach((version) => {
                 notification.info({
                     ns: 'app.utils',
@@ -87,15 +85,16 @@
                     }
                 }, -1);
             });
+            // addVersions(newVersionList);
 
-            addVersions(newVersionList);
             user.setSetting('lastOpenVersion', WavesApp.version);
+            user.setSetting('whatsNewList', newVersionList);
         });
 
         return Object.create(null);
     };
 
-    factory.$inject = ['notification', 'user', 'migration', 'utils'];
+    factory.$inject = ['notification', 'user', 'migration'];
 
     angular.module('app.utils').factory('whatsNew', factory);
 })();
