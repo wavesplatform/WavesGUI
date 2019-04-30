157d46af222343cb522bd14ffa61256b5a71c8a2
ekomarovskaya <ekomarovskaya@wavesplatform.com>
2019-04-30 15:08:08 +0300

    CLIENT-1784: stand update 2

--- src/modules/stand/controller/StandCtrl.js
+++ src/modules/stand/controller/StandCtrl.js
@@ -7,7 +7,7 @@
      * @param $scope
      * @return {StandCtrl}
      */
-    const controller = function (Base, $scope) {
+        const controller = function (Base, $scope, userNotification, notification) {
 
         class StandCtrl extends Base {
 
@@ -37,6 +37,75 @@
                  * @type {string}
                  */
                 this.seed = seed;
+
+                const loop = (index) => {
+                    userNotification.info({
+                        body: {
+                            literal: index + 1
+                        }
+                    }).then(() => loop(index + 1));
+                };
+
+                loop(0);
+
+                const loop2 = (index) => {
+                    userNotification.error({
+                        body: {
+                            literal: index + 1
+                        }
+                    }).then(() => loop2(index + 1));
+                };
+
+                loop2(0);
+
+                const loop3 = (index) => {
+                    userNotification.success({
+                        body: {
+                            literal: index + 1
+                        }
+                    }).then(() => loop3(index + 1));
+                };
+
+                loop3(0);
+
+                const loop4 = (index) => {
+                    userNotification.warn({
+                        body: {
+                            literal: index + 1
+                        }
+                    }).then(() => loop4(index + 1));
+                };
+
+                loop4(0);
+
+                const loop0 = (index) => {
+                    notification.info({
+                        title: {
+                            literal: 'Title info'
+                        },
+                        body: {
+                            literal: 'Text body info'
+                        }
+                    }).then(() => loop0(index + 1));
+                };
+
+                loop0(0);
+
+
+                const loop5 = (index) => {
+                    notification.error({
+                        title: {
+                            literal: 'Title error'
+                        },
+                        body: {
+                            literal: 'Text body error'
+                        }
+                    }).then(() => loop5(index + 1));
+                };
+
+                loop5(0);
+
+
             }
 
         }
@@ -44,7 +113,7 @@
         return new StandCtrl();
     };
 
-    controller.$inject = ['Base', '$scope'];
+    controller.$inject = ['Base', '$scope', 'userNotification', 'notification'];
 
     angular.module('app.stand').controller('StandCtrl', controller);
 })();
