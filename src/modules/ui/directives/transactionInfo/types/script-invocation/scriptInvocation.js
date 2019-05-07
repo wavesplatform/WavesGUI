ce40e99effdcbe8398723f20327061d7ce1569c6
Max Axenov <maksenov@wavesplatform.com>
2019-05-07 14:28:13 +0300

    CLIENT-1690: edit show btn literal, copy json

--- src/modules/ui/directives/transactionInfo/types/script-invocation/scriptInvocation.js
+++ src/modules/ui/directives/transactionInfo/types/script-invocation/scriptInvocation.js
@@ -5,7 +5,7 @@
      * @param {$rootScope.Scope} $scope
      * @return {DataInfo}
      */
-    const controller = function ($scope) {
+    const controller = function () {
 
         class DataInfo {
 
@@ -25,25 +25,25 @@
              * @type {string}
              */
             function;
+            /**
+             * @type {string}
+             */
+            json;
 
 
             $postLink() {
                 this.transaction = this.signable.getTxData();
                 this.args = this.transaction.call.args;
                 this.function = this.transaction.call.function;
-                (this.transaction.id ? Promise.resolve(this.transaction.id) : this.signable.getId())
-                    .then(id => {
-                        this.id = id;
-                        $scope.$apply();
-                    });
+                this.signable.getDataForApi().then(json => {
+                    this.json = WavesApp.stringifyJSON(json, null, 4);
+                });
             }
 
         }
 
         return new DataInfo();
     };
-
-    controller.$inject = ['$scope'];
 
     angular.module('app.ui').component('wScriptInvocationInfo', {
         bindings: {
