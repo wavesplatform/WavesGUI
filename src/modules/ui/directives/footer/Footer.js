290ee2851f739dcdd0c0ebfadfbca814c6f12d03
Max Axenov <maksenov@wavesplatform.com>
2019-05-27 19:14:07 +0300

    CLIENT-1842: animation toaster

--- src/modules/ui/directives/footer/Footer.js
+++ src/modules/ui/directives/footer/Footer.js
@@ -1,13 +1,30 @@
 (function () {
     'use strict';
 
-    const controller = function (Base) {
+    const controller = function (Base, $element) {
 
         class FooterCtrl extends Base {
+
+            /**
+             * @public
+             * @type {string}
+             */
+            link;
 
             constructor() {
                 super();
                 this.hovered = false;
+
+                this.link = navigator.userAgent.match(/iPhone|iPad|iPod/i) ?
+                    'https://itunes.apple.com/us/app/waves-wallet/id1233158971?mt=8' :
+                    'https://play.google.com/store/apps/details?id=com.wavesplatform.wallet';
+            }
+
+            /**
+             * @public
+             */
+            hideToaster() {
+                $element.find('.toaster-mobiles').addClass('hidden-toaster');
             }
 
         }
@@ -15,7 +32,7 @@
         return new FooterCtrl();
     };
 
-    controller.$inject = ['Base'];
+    controller.$inject = ['Base', '$element'];
 
     angular.module('app.ui').component('wFooter', {
         templateUrl: 'modules/ui/directives/footer/footer.html',
