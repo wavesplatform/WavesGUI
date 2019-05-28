eabc64a998a656f42784516492fc55c5c7c685ba
Max Axenov <maksenov@wavesplatform.com>
2019-05-28 15:10:01 +0300

    CLIENT-1842: roadmap init

--- src/modules/ui/directives/siteHeader/SiteHeader.js
+++ src/modules/ui/directives/siteHeader/SiteHeader.js
@@ -41,6 +41,11 @@
                 this._initClickHandlers();
             }
 
+            $onDestroy() {
+                super.$onDestroy();
+                $element.find('.mobile-menu-fader, .mobile-menu-toggler').off();
+            }
+
             open(sref) {
                 if (this.isLogined) {
                     $state.go(sref);
