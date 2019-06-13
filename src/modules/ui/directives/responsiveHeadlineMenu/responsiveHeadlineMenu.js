aec213e75d0a0685b0747d25840116cf2896bcf3
uiskander <theodsgn@gmail.com>
2019-06-13 20:04:36 +0300

    DEXW-1794: add responsive- menu component

---
+++ src/modules/ui/directives/responsiveHeadlineMenu/responsiveHeadlineMenu.js
@@ -0,0 +1,214 @@
+(function () {
+    'use strict';
+
+    /**
+     * @param Base
+     * @param {StateManager} stateManager
+     * @param {ModalManager} modalManager
+     * @param {app.utils} utils
+     * @param $scope
+     * @param {User} user
+     * @param {$state} $state
+     * @param {JQuery} $document
+     * @param {JQuery} $element
+     * @return {SiteHeader}
+     */
+    const controller = function (Base,
+                                 stateManager,
+                                 modalManager,
+                                 user,
+                                 $state,
+                                 $element,
+                                 $document,
+                                 utils,
+                                 $scope) {
+
+        class SiteHeaderCtrl extends Base {
+
+            /**
+             * @public
+             * @type {string}
+             */
+            userName;
+            /**
+             * @private
+             * @type {Array}
+             */
+            userList = [];
+
+            constructor() {
+                super($scope);
+                this.hovered = false;
+                this.address = user.address || '3PHBX4uXhCyaANUxccLHNXw3sqyksV7YnDz';
+                this.isLogined = !!user.address;
+                this.userName = user.name;
+                this.receive(stateManager.changeRouteState, () => {
+                    this.subStateList = stateManager.subStateList;
+                    this.rootStateList = stateManager.rootStateList;
+                });
+                this.rootStateList = stateManager.rootStateList;
+                this.subStateList = stateManager.subStateList;
+                this.menuList = stateManager.getStateTree();
+                this.activeState = $state.$current.name.slice($state.$current.name.lastIndexOf('.') + 1);
+                this.userType = user.userType;
+                if (!this.isLogined) {
+                    this.activeState = this.activeState.replace('-demo', '');
+                }
+
+                this.isDesktop = WavesApp.isDesktop();
+
+                this.isScript = user.hasScript();
+                this.isKeeper = user.userType === 'wavesKeeper';
+                this.isLedger = user.userType === 'ledger';
+
+                this.hasTypeHelp = this.isScript && (this.isLedger || this.isKeeper);
+
+                user.getFilteredUserList().then(list => {
+                    this.userList = list;
+                    this.hasUsers = this.userList.length > 0;
+                    utils.postDigest($scope).then(() => {
+                        this._initFader();
+                        $scope.$apply();
+                    });
+                });
+
+                this._initClickHandlers();
+            }
+
+            $onDestroy() {
+                super.$onDestroy();
+                $element.find('.mobile-menu-fader, .mobile-menu-toggler').off();
+            }
+
+            open(sref) {
+                if (this.isLogined) {
+                    $state.go(sref);
+                } else {
+                    this._getDialogModal(`open-${sref}`, () => $state.go('welcome'), () => $state.go('create'));
+                }
+            }
+
+            logout() {
+                user.logout();
+            }
+
+            avatarClick() {
+                $document.find('body').removeClass('menu-is-shown');
+                if (this.isLogined) {
+                    modalManager.showAccountInfo();
+                } else {
+                    this._getDialogModal('account', () => $state.go('welcome'), () => $state.go('create'));
+                }
+            }
+
+            settings() {
+                $document.find('body').removeClass('menu-is-shown');
+                if (this.isLogined) {
+                    modalManager.showSettings();
+                } else {
+                    this._getDialogModal('settings', () => $state.go('welcome'), () => $state.go('create'));
+                }
+            }
+
+            /**
+             * public
+             */
+            toWelcome() {
+                if (this.isLogined) {
+                    return modalManager.showConfirmLogout().then(() => {
+                        user.logout('welcome');
+                    });
+                }
+            }
+
+            /**
+             * public
+             */
+            removeInnerMenu() {
+                $document.find('w-site-header header').removeClass('show-wallet show-aliases show-downloads');
+            }
+
+            /**
+             * public
+             */
+            removeBodyClass() {
+                $document.find('body').removeClass('menu-is-shown');
+            }
+
+            /**
+             * @param type
+             * @param success
+             * @param error
+             * @return {Promise}
+             * @private
+             */
+            _getDialogModal(type, success, error) {
+                return modalManager.showDialogModal({
+                    iconClass: `${type.replace(/\./g, '-')}-account-info`,
+                    message: { literal: `modal.${type}.message` },
+                    buttons: [
+                        {
+                            success: false,
+                            classes: 'big',
+                            text: { literal: `modal.${type}.cancel` },
+                            click: error
+                        },
+                        {
+                            success: true,
+                            classes: 'big submit',
+                            text: { literal: `modal.${type}.ok` },
+                            click: success
+                        }
+                    ]
+                });
+            }
+
+            /**
+             * @private
+             */
+            _initClickHandlers() {
+                $element.find('.mobile-menu-fader, .mobile-menu-toggler').on('click', () => {
+                    $element.find('header').toggleClass('expanded');
+                    $document.find('body').toggleClass('menu-is-shown');
+                });
+            }
+
+            /**
+             * @private
+             */
+            _initFader() {
+                $element.find('.dropdown-toggler').on('mouseover', () => {
+                    $element.find('.dropdown-fader').addClass('show-fader');
+                });
+                $element.find('.dropdown-toggler').on('mouseleave', () => {
+                    $element.find('.dropdown-fader').removeClass('show-fader');
+                });
+            }
+
+        }
+
+        return new SiteHeaderCtrl();
+    };
+
+    controller.$inject = [
+        'Base',
+        'stateManager',
+        'modalManager',
+        'user',
+        '$state',
+        '$element',
+        '$document',
+        'utils',
+        '$scope'
+    ];
+
+    angular.module('app.ui').component('wResponsiveHeadlineMenu', {
+        bindings: {
+            signInBtn: '<',
+            getStartedBtn: '<'
+        },
+        templateUrl: 'modules/ui/directives/responsiveHeadlineMenu/responsiveHeadlineMenu.html',
+        transclude: false,
+        controller
+    });
+})();
