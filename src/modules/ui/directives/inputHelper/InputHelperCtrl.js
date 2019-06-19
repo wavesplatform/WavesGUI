7a3c9f5152267c0b97f2304251df6c938cf85573
Daniil <tsddaniil@gmail.com>
2019-06-19 07:23:57 +0300

    Update InputHelperCtrl.js

--- src/modules/ui/directives/inputHelper/InputHelperCtrl.js
+++ src/modules/ui/directives/inputHelper/InputHelperCtrl.js
@@ -1,21 +1,9 @@
 (function () {
     'use strict';
 
-    const controller = function (Base) {
-
-        class inputHelperController extends Base {
-
-        }
-
-        return new inputHelperController();
-    };
-
-    controller.$inject = ['Base'];
-
     angular.module('app.ui').component('wInputHelper', {
         templateUrl: 'modules/ui/directives/inputHelper/inputHelper.html',
-        transclude: true,
-        controller
+        transclude: true
     });
 
 })();
