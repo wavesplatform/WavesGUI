d55dcadcb265559917dcdd57aa6e86b663b9be3b
finico <agonchar@wavesplatform.com>
2019-06-21 12:37:32 +0300

    DEXW-1115: Refactor Receive modal

---
+++ src/modules/utils/modals/receive/receiveCryptocurrency/ReceiveCryptocurrency.js
@@ -0,0 +1,111 @@
+(function () {
+    'use strict';
+
+    /**
+     * @param {typeof Base} Base
+     * @param {ng.IScope} $scope
+     * @param {GatewayService} gatewayService
+     * @param {User} user
+     * @return {ReceiveCryptocurrency}
+     */
+    const controller = function (Base, $scope, gatewayService, user) {
+
+        /**
+         * @extends {ng.IController}
+         */
+        class ReceiveCryptocurrency extends Base {
+
+            /**
+             * @type {Asset}
+             */
+            asset = null;
+
+            /**
+             * @type {Array}
+             */
+            cryptocurrencies;
+
+            /**
+             * @type {boolean}
+             */
+            isSingleAsset;
+
+            /**
+             * @type {string}
+             */
+            assetKeyName;
+
+            /**
+             * @type {string}
+             */
+            gatewayAddress;
+
+            /**
+             * @type {boolean}
+             */
+            gatewayServerError = false
+
+            /**
+             * @type {boolean}
+             */
+            isVostok;
+
+            /**
+             * @type {string}
+             */
+            chosenAssetId = null;
+
+            /**
+             * @type {Function}
+             */
+            onAssetChange;
+
+            constructor() {
+                super();
+
+                this.observe('chosenAssetId', ({ value: id }) => this.onAssetChange({ id }));
+
+                this.observe('asset', this.updateGatewayAddress);
+            }
+
+            /**
+             * @public
+             */
+            updateGatewayAddress() {
+                this.gatewayServerError = false;
+
+                const depositDetails = gatewayService.getDepositDetails(this.asset, user.address);
+
+                if (depositDetails) {
+                    depositDetails.then((details) => {
+                        this.gatewayAddress = details.address;
+                        $scope.$apply();
+                    }, () => {
+                        this.gatewayAddress = null;
+                        this.gatewayServerError = true;
+                        $scope.$apply();
+                    });
+
+                    this.assetKeyName = gatewayService.getAssetKeyName(this.asset, 'deposit');
+                    this.isVostok = this.asset.id === WavesApp.defaultAssets.VST;
+                }
+            }
+
+        }
+
+        return new ReceiveCryptocurrency();
+    };
+
+    controller.$inject = ['Base', '$scope', 'gatewayService', 'user'];
+
+    angular.module('app.utils').component('wReceiveCryptocurrency', {
+        controller,
+        bindings: {
+            asset: '<',
+            cryptocurrencies: '<',
+            isSingleAsset: '<',
+            onAssetChange: '&'
+        },
+        templateUrl: 'modules/utils/modals/receive/receiveCryptocurrency/receive-cryptocurrency.html'
+    });
+})();
