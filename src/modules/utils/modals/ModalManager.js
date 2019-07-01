0a2e1da9aa6b1b7cbb322ee42dab6dc9e075f628
ekomarovskaya <ekomarovskaya@wavesplatform.com>
2019-07-01 11:31:16 +0300

    DEXW-1854: dexw-1854 bem asset info head

--- src/modules/utils/modals/ModalManager.js
+++ src/modules/utils/modals/ModalManager.js
@@ -175,7 +175,7 @@
                     contentUrl: 'modules/utils/modals/assetInfo/assetInfo.html',
                     locals: asset,
                     controller: 'AssetInfoCtrl',
-                    mod: 'm-dialog m-dialog-assetInfo'
+                    mod: 'm-dialog m-dialog-asset-info'
                 });
             }
 
