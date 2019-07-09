528bbcad40f604990569b60cb0064f07a0f344ab
finico <agonchar@wavesplatform.com>
2019-07-09 16:43:41 +0300

    DEXW-1907: Fix sorting of watchlist by price

--- src/modules/dex/directives/dexWatchlist/WatchList.js
+++ src/modules/dex/directives/dexWatchlist/WatchList.js
@@ -173,7 +173,7 @@
                     {
                         id: 'price',
                         title: { literal: 'directives.watchlist.price' },
-                        sort: this._getComparatorByPath('price')
+                        sort: this._getComparatorByPath('lastPrice')
                     },
                     {
                         id: 'change',
