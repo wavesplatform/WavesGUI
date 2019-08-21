485f62037b93785087eac5ced0e1b39fee8a477f
Max Axenov <maksenov@wavesplatform.com>
2019-08-21 11:54:58 +0300

    DEXW-1884: fix logic for nice number

--- src/modules/utils/services/utils.js
+++ src/modules/utils/services/utils.js
@@ -1114,9 +1114,10 @@
                 const bigNum = this.parseNiceNumber(num);
                 const formatted = this.getNiceNumber(bigNum, precision);
 
-                const shortModeCondition = typeof shortMode === 'number' ? bigNum.gte(shortMode) : bigNum.gte(10000);
+                const isShortMode = typeof shortMode === 'number' ? true : shortMode;
+                const minValue = typeof shortMode === 'number' ? shortMode : 10000;
 
-                if (shortMode && shortModeCondition) {
+                if (isShortMode && bigNum.gte(minValue)) {
                     return this.getNiceBigNumberTemplate(bigNum);
                 } else {
                     const separatorDecimal = WavesApp.getLocaleData().separators.decimal;
