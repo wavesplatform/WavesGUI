6ed9e133bcff033ce9eb4b8331d8db2e648024a6
okanishcheva <okanishcheva@wavesplatform.com>
2019-08-22 11:10:33 +0300

    DEXW-2042: refactor coinomat api

--- src/modules/utils/modals/receive/receiveCryptocurrency/ReceiveCryptocurrency.js
+++ src/modules/utils/modals/receive/receiveCryptocurrency/ReceiveCryptocurrency.js
@@ -81,7 +81,7 @@
                 if (depositDetails) {
                     depositDetails.then((details) => {
                         this.gatewayAddress = details.address;
-                        this.minAmount = Money.fromTokens(this._getMin(details), this.asset);
+                        this.minAmount = Money.fromTokens(details.minimumAmount, this.asset);
                         $scope.$apply();
                     }, () => {
                         this.minAmount = Money.fromTokens(0.001, this.asset);
@@ -93,21 +93,6 @@
                     this.assetKeyName = gatewayService.getAssetKeyName(this.asset, 'deposit');
                     this.isVostok = this.asset.id === WavesApp.defaultAssets.VST;
                 }
-            }
-
-            /**
-             * @param {object} data
-             * @return {number | BigNumber}
-             * @private
-             */
-            _getMin(data) {
-                if (data.details && data.details.in_min) {
-                    return data.details.in_min;
-                }
-                if (data.minimumAmount) {
-                    return data.minimumAmount;
-                }
-                return 0.001;
             }
 
         }
