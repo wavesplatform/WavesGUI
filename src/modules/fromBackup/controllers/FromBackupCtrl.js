e415930f406b1309d59205b56e1351b6b079543a
vba2000 <vba@mail.ru>
2019-09-24 14:27:49 +0300

    DEXW-2133: add import page

---
+++ src/modules/fromBackup/controllers/FromBackupCtrl.js
@@ -0,0 +1,116 @@
+(function () {
+    'use strict';
+
+    const { libs } = require('@waves/waves-transactions');
+
+    /**
+     * @param {typeof Base} Base
+     * @param {ng.IScope} $scope
+     * @param {*} $state
+     * @param {User} user
+     * @param {app.utils} utils
+     * @return {FromBackupCtrl}
+     */
+    const controller = function (Base, $scope, $state, user, utils) {
+
+        const analytics = require('@waves/event-sender');
+
+        class FromBackupCtrl extends Base {
+
+            /**
+             * @type {number}
+             */
+            step = 0;
+
+            constructor() {
+                super($scope);
+                analytics.send({ name: 'Import From Backup Click', target: 'ui' });
+            }
+
+            static parseUsers(data) {
+                const backup = JSON.parse(data);
+
+                if (!backup || backup.type !== 'wavesBackup' || !backup.lastOpenVersion) {
+                    throw new Error('wrongFile');
+                }
+
+                const dataObject = JSON.parse(
+                    libs.crypto.bytesToString(libs.crypto.base64Decode(backup.data))
+                );
+
+                return {
+                    ...dataObject,
+                    type: backup.type,
+                    lastOpenVersion: backup.lastOpenVersion
+                };
+            }
+
+            $onDestroy() {
+                super.$onDestroy();
+            }
+
+            onFileChange(event) {
+                const reader = new FileReader();
+                if (event.target.files && event.target.files.length > 0) {
+                    const file = event.target.files[0];
+                    reader.readAsDataURL(file);
+                    reader.onload = () => {
+                        try {
+                            const fileData = libs.crypto
+                                .bytesToString(libs.crypto.base64Decode(reader.result.split(',')[1]));
+                            this.backup = FromBackupCtrl.parseUsers(fileData);
+                            this.readError = null;
+
+                        } catch (e) {
+                            this.readError = 'wrongFile';
+                        }
+
+                        utils.safeApply($scope);
+                    };
+                }
+            }
+
+            // /**
+            //  * @return {void}
+            //  */
+            // login() {
+            //     const newUser = {
+            //         ...this.selectedUser,
+            //         userType: this.adapter.type,
+            //         networkByte: WavesApp.network.code.charCodeAt(0)
+            //     };
+            //
+            //     return user.create(newUser, true, true).then(() => {
+            //         $state.go(user.getActiveState('wallet'));
+            //     }).catch(() => {
+            //         this.error = true;
+            //         $scope.$digest();
+            //     });
+            // }
+
+        }
+
+        return new FromBackupCtrl();
+    };
+
+    controller.$inject = ['Base', '$scope', '$state', 'user', 'utils'];
+
+    angular.module('app.fromBackup').controller('FromBackupCtrl', controller);
+    angular.module('app.fromBackup').directive('fileChange', ['$parse', function ($parse) {
+
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function ($scope, element, attrs) {
+
+                const attrHandler = $parse(attrs.fileChange);
+                const handler = function (e) {
+                    $scope.$apply(function () {
+                        attrHandler($scope, { $event: e, files: e.target.files });
+                    });
+                };
+                element[0].addEventListener('change', handler, false);
+            }
+        };
+    }]);
+})();
--- src/modules/utils/modals/exportAccounts/ExportAccounts.js
+++ src/modules/utils/modals/exportAccounts/ExportAccounts.js
@@ -10,19 +10,44 @@
      */
     const controller = function (Base, $scope, user, utils) {
 
+        const { encryptSeed } = require('@waves/waves-transactions').seedUtils;
+        const { crypto } = require('@waves/waves-transactions').libs;
+
         class ExportAccountsCtrl extends Base {
 
+            /**
+             * @type {{}}
+             */
             checkedHash = {};
+            /**
+             * @type {boolean}
+             */
+            needPassword = false;
+            /**
+             * @type {Array}
+             */
+            userlist = [];
+            /**
+             * @type {string}
+             */
+            password = '';
+            /**
+             * @type {{ lastOpenVersion: string }}
+             */
+            settings = { lastOpenVersion: null };
 
             constructor() {
                 super($scope);
                 this.settings = user.getMultiAccountSettings();
                 this.userList = [];
                 this.checkedHash[user.address] = true;
-
+                user.getMultiAccountSettings().then(settings => {
+                    this.settings = settings;
+                });
                 user.getMultiAccountUsers().then(
                     (users) => {
-                        this.userList = users;
+                        this.userList = Array.isArray(users) ? users : [];
+                        this.onSelect();
                         utils.safeApply($scope);
                     }
                 );
@@ -30,12 +55,42 @@
 
             toggleSelect(address) {
                 this.checkedHash[address] = !this.checkedHash[address];
+                this.onSelect();
+            }
+
+            onSelect() {
+                this.hasSelected = !!Object.values(this.checkedHash).filter(Boolean).length;
             }
 
             selectAll() {
                 this.userList.forEach((user) => {
                     this.checkedHash[user.address] = true;
                 });
+                this.onSelect();
+            }
+
+            onSubmit() {
+                const users = Object.entries(this.checkedHash)
+                    .filter(item => item[1])
+                    .map(([address]) => this.userList.find(user => user.address === address));
+                const stringifyUsers = JSON.stringify(users);
+                const saveUsers = !this.needPassword ?
+                    stringifyUsers :
+                    encryptSeed(stringifyUsers, this.password, 10000);
+
+                const saveData = {
+                    encrypted: this.needPassword,
+                    encryptionRounds: this.needPassword ? 10000 : undefined,
+                    saveUsers
+                };
+
+                const toSave = {
+                    type: 'wavesBackup',
+                    lastOpenVersion: this.settings.lastOpenVersion,
+                    data: crypto.base64Encode(crypto.stringToBytes(JSON.stringify(saveData)))
+                };
+
+                utils.downloadFile(`accountsBackup-${Date.now()}.json`, JSON.stringify(toSave, null, 4));
             }
 
         }
