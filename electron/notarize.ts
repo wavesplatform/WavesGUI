949b26fa3fc86f05267eccdd9be13b1e93c87844
Marat Tuktarov <mtuktarov@yahoo.com>
2019-10-20 16:40:53 +0300

    templateFix

--- electron/notarize.ts
+++ electron/notarize.ts
@@ -12,7 +12,7 @@ process.on('uncaughtException', function(err) {
 async function notarizeAppInfo() {
   const { stdout, stderr } = await exec("\
     { sleep 30 && xcrun altool --notarization-history 0 -u '${appleIdUsername}' -p '${appleIdPassword}' || true ;} && \
-    { xcrun altool --notarization-info \$(cat out.log | grep 'checking notarization status' | cut -d\: -f2 | cut -d' ' -f2 | \
+    { xcrun altool --notarization-info \$(cat out.log | grep 'checking notarization status' | cut -d':' -f2 | cut -d' ' -f2 | \
         tail -1) -u '${appleIdUsername}' -p '${appleIdPassword}' || true ;} && \
     { xcrun stapler staple -v './release/mainnet/mac/Waves DEX.app' || true ;}");
     console.log(`\${stdout}`);
