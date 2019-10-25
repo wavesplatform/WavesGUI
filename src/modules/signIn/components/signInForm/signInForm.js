0a1e3bf6e049338a4a46bcfe8434d86b50ebec9c
finico <agonchar@web3tech.ru>
2019-10-25 16:47:32 +0300

    DEXW-2225: Refactor SignInForm

--- src/modules/signIn/components/signInForm/signInForm.js
+++ src/modules/signIn/components/signInForm/signInForm.js
@@ -1,106 +1,101 @@
 (() => {
     'use strict';
 
-    /**
-     * @param {typeof Base} Base
-     * @param {ng.IScope} $scope
-     * @param {*} $state
-     * @param {User} user
-     * @param {MultiAccount} multiAccount
-     * @param {ModalManager} modalManager
-     * @returns {SignInFormCtrl}
-     */
-    const controller = function (Base, $scope, $state, user, multiAccount, modalManager) {
+    class SignInFormCtrl {
 
-        class SignInFormCtrl extends Base {
+        static $inject = ['$state', 'user', 'multiAccount', 'modalManager'];
 
-            /**
-             * @type {string}
-             */
-            password = '';
-            /**
-             * @type {ng.IFormController|null}
-             */
-            form = null;
-            /**
-             * @type {string}
-             */
-            multiAccountData = '';
-            /**
-             * @type {string}
-             */
-            multiAccountHash = '';
-            /**
-             * @type {Array|null}
-             */
-            legacyUserList = null;
+        /**
+         * @type {string}
+         */
+        password = '';
+        /**
+         * @type {ng.IFormController|null}
+         */
+        form = null;
+        /**
+         * @type {string}
+         */
+        multiAccountData = '';
+        /**
+         * @type {string}
+         */
+        multiAccountHash = '';
+        /**
+         * @type {Array|null}
+         */
+        legacyUserList = null;
 
-            constructor() {
-                super($scope);
-
-                this.observe('password', this._updatePassword);
-
-                Promise.all([
-                    user.getMultiAccountData(),
-                    user.getMultiAccountHash(),
-                    user.getFilteredUserList()
-                ]).then(([multiAccountData, multiAccountHash, userList]) => {
-                    if (!multiAccountData) {
-                        $state.go('signUp');
-                    } else {
-                        this.multiAccountData = multiAccountData;
-                        this.multiAccountHash = multiAccountHash;
-                        this.legacyUserList = userList;
-                    }
-                });
-            }
-
-            onSubmit() {
-                if (this.form.$invalid) {
-                    return;
-                }
-
-                this.showPasswordError = false;
-
-                multiAccount.signIn(
-                    this.multiAccountData,
-                    this.password,
-                    undefined,
-                    this.multiAccountHash
-                ).then(() => {
-                    this.onSuccess();
-                }).catch(() => {
-                    this._showPasswordError();
-                });
-            }
-
-            showForgotPasswordModal() {
-                modalManager.showForgotPasswordModal().then(() => {
-                    this.onResetPassword();
-                });
-            }
-
-            _showPasswordError() {
-                this.password = '';
-                this.showPasswordError = true;
-            }
-
-            _updatePassword() {
-                if (this.password) {
-                    this.showPasswordError = false;
-                }
-            }
-
+        /**
+         * @param {*} $state
+         * @param {User} user
+         * @param {MultiAccount} multiAccount
+         * @param {ModalManager} modalManager
+         */
+        constructor($state, user, multiAccount, modalManager) {
+            this.$state = $state;
+            this.user = user;
+            this.multiAccount = multiAccount;
+            this.modalManager = modalManager;
         }
 
-        return new SignInFormCtrl();
-    };
+        $onInit() {
+            Promise.all([
+                this.user.getMultiAccountData(),
+                this.user.getMultiAccountHash(),
+                this.user.getFilteredUserList()
+            ]).then(([multiAccountData, multiAccountHash, userList]) => {
+                if (!multiAccountData) {
+                    this.$state.go('signUp');
+                } else {
+                    this.multiAccountData = multiAccountData;
+                    this.multiAccountHash = multiAccountHash;
+                    this.legacyUserList = userList;
+                }
+            });
+        }
 
-    controller.$inject = ['Base', '$scope', '$state', 'user', 'multiAccount', 'modalManager'];
+        onSubmit() {
+            if (this.form.$invalid) {
+                return;
+            }
+
+            this.showPasswordError = false;
+
+            this.multiAccount.signIn(
+                this.multiAccountData,
+                this.password,
+                undefined,
+                this.multiAccountHash
+            ).then(() => {
+                this.onSuccess();
+            }).catch(() => {
+                this._showPasswordError();
+            });
+        }
+
+        showForgotPasswordModal() {
+            this.modalManager.showForgotPasswordModal().then(() => {
+                this.onResetPassword();
+            });
+        }
+
+        onPasswordChange() {
+            if (this.password) {
+                this.showPasswordError = false;
+            }
+        }
+
+        _showPasswordError() {
+            this.password = '';
+            this.showPasswordError = true;
+        }
+
+    }
 
     angular.module('app.signIn').component('wSignInForm', {
         templateUrl: 'modules/signIn/components/signInForm/signInForm.html',
-        controller,
+        controller: SignInFormCtrl,
         bindings: {
             onSuccess: '&',
             onResetPassword: '&'
