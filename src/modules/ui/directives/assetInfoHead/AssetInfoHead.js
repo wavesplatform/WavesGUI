80328bbb5be84a33be74608233bf0fc23f037e15
Max Axenov <maksenov@wavesplatform.com>
2019-10-28 12:38:48 +0300

    DEXW-2114: fix: assetInfo rating

--- src/modules/ui/directives/assetInfoHead/AssetInfoHead.js
+++ src/modules/ui/directives/assetInfoHead/AssetInfoHead.js
@@ -48,7 +48,7 @@
                     this.ticker = asset.ticker;
                     const { hasLabel, isGateway } = utils.getDataFromOracles(asset.id);
                     this.hasLabel = hasLabel;
-                    this.isGatewayOrWaves = isGateway;
+                    this.isGatewayOrWaves = this.assetId === WavesApp.defaultAssets.WAVES || isGateway;
                     $scope.$apply();
                 });
 
