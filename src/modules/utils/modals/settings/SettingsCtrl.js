2142fcce0d41595eee2f773dd18e8fa0b9e7dbfc
okanishcheva <okanishcheva@wavesplatform.com>
2019-10-28 16:52:22 +0300

    DEXW-2151: fix set default assetsOracleTmp

--- src/modules/utils/modals/settings/SettingsCtrl.js
+++ src/modules/utils/modals/settings/SettingsCtrl.js
@@ -253,6 +253,7 @@
                 this.scamListUrl = WavesApp.network.scamListUrl;
                 this.tokensNameListUrl = WavesApp.network.tokensNameListUrl;
                 this.oracleWaves = WavesApp.oracles.waves;
+                this.assetsOracleTmp = this.oracleWaves;
                 this.api = WavesApp.network.api;
             }
 
